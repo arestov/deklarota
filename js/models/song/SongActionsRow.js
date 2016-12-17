bce6e6be87354585522416534256397f2263492c
Gleb Arestov <gr1b0k@gmail.com>
2016-12-17 08:57:27 +0300

    DRAFTLY collect probes

--- js/models/song/SongActionsRow.js
+++ js/models/song/SongActionsRow.js
@@ -11,9 +11,9 @@ var SongActSharing = require('./SongActSharing');
 var pvUpdate = pv.update;
 
 var LfmLoveIt = spv.inh(LfmAuth.LfmLogin, {
-  init: function(target) {
-    target.song = target.map_parent.mo;
-  }
+	init: function(target) {
+		target.song = target.map_parent.mo;
+	}
 }, {
   "+states": {
     "access_desc": [
@@ -22,72 +22,72 @@ var LfmLoveIt = spv.inh(LfmAuth.LfmLogin, {
     ]
   },
 
-  beforeRequest: function() {
-    this.bindAuthCallback();
-  },
+	beforeRequest: function() {
+		this.bindAuthCallback();
+	},
 
-  act: function () {
-    this.makeLove();
-  },
+	act: function () {
+		this.makeLove();
+	},
 
-  bindAuthCallback: function(){
-    pvUpdate(this.app, 'lfm_auth_request', this);
-  },
+	bindAuthCallback: function(){
+		pvUpdate(this.app, 'lfm_auth_request', this);
+	},
 
-  makeLove: function() {
+	makeLove: function() {
 
-    if (this.app.lfm.sk){
-      var _this = this;
-      pv.update(this, 'wait_love_done', true);
-      this.app.lfm.post('Track.love', {
-        sk: this.app.lfm.sk,
-        artist: this.song.state('artist'),
-        track: this.song.state('track')
-      }).then(anyway, anyway);
+		if (this.app.lfm.sk){
+			var _this = this;
+			pv.update(this, 'wait_love_done', true);
+			this.app.lfm.post('Track.love', {
+				sk: this.app.lfm.sk,
+				artist: this.song.state('artist'),
+				track: this.song.state('track')
+			}).then(anyway, anyway);
 
-      function anyway(){
-        pv.update(_this, 'wait_love_done', false);
-        _this.trigger('love-success');
-      }
+			function anyway(){
+				pv.update(_this, 'wait_love_done', false);
+				_this.trigger('love-success');
+			}
 
-      this.app.trackEvent('song actions', 'love');
-    }
+			this.app.trackEvent('song actions', 'love');
+		}
 
 
-  }
+	}
 });
 var LoveRow = spv.inh(comd.BaseCRow, {
-  init: function(target){
-    target.mo = target.map_parent.map_parent;
+	init: function(target){
+		target.mo = target.map_parent.map_parent;
 
-    var old_lit = null;
-    var hide_on_love = function() {
-      target.hide();
-    };
-    target.on('child_change-lfm_loveit', function(e) {
-      if (old_lit) {
-        old_lit.off('love-success', hide_on_love);
-      }
+		var old_lit = null;
+		var hide_on_love = function() {
+			target.hide();
+		};
+		target.on('child_change-lfm_loveit', function(e) {
+			if (old_lit) {
+				old_lit.off('love-success', hide_on_love);
+			}
 
-      if (e.value) {
-        e.value.on('love-success', hide_on_love);
-      }
-      old_lit = e.value;
-    });
+			if (e.value) {
+				e.value.on('love-success', hide_on_love);
+			}
+			old_lit = e.value;
+		});
 
-  }
+	}
 }, {
-  actionsrow_src: '^',
-  'nest-lfm_loveit': [LfmLoveIt, {
-    idle_until: 'active_view'
-  }],//ver important to not init this each song selected
-  model_name: 'row-love'
+	actionsrow_src: '^',
+	'nest-lfm_loveit': [LfmLoveIt, {
+		idle_until: 'active_view'
+	}],//ver important to not init this each song selected
+	model_name: 'row-love'
 });
 
 var ScrobbleRow = spv.inh(comd.BaseCRow, {}, {
-  actionsrow_src: '^',
-  'nest-lfm_scrobble': [LfmAuth.LfmScrobble],
-  model_name: 'row-lastfm'
+	actionsrow_src: '^',
+	'nest-lfm_scrobble': [LfmAuth.LfmScrobble],
+	model_name: 'row-lastfm'
 });
 
 var ShuffleListRow = spv.inh(comd.BaseCRow, {}, {
@@ -98,12 +98,12 @@ var ShuffleListRow = spv.inh(comd.BaseCRow, {}, {
     ]
   },
 
-  actionsrow_src: '^',
-  model_name: 'row-pl-shuffle',
+	actionsrow_src: '^',
+	model_name: 'row-pl-shuffle',
 
-  switchSetting: function(state) {
-    this.app.setSetting('pl-shuffle', state);
-  }
+	switchSetting: function(state) {
+		this.app.setSetting('pl-shuffle', state);
+	}
 });
 
 var RepeatSongRow = spv.inh(comd.BaseCRow, {}, {
@@ -114,33 +114,33 @@ var RepeatSongRow = spv.inh(comd.BaseCRow, {}, {
     ]
   },
 
-  actionsrow_src: '^',
-  model_name: 'row-repeat-song',
+	actionsrow_src: '^',
+	model_name: 'row-repeat-song',
 
-  switchSetting: function(state) {
-    this.app.setSetting('rept-song', state);
-  }
+	switchSetting: function(state) {
+		this.app.setSetting('rept-song', state);
+	}
 });
 
 var constrs = [ScrobbleRow, RepeatSongRow, ShuffleListRow, SongActPlaylisting, SongActSharing, LoveRow, SongActTaging];
 
 var parts_storage = {};
 constrs.forEach(function(el) {
-  parts_storage[el.prototype.model_name] = el;
+	parts_storage[el.prototype.model_name] = el;
 });
 
 var constrs_names= constrs.map(function(el) {
-  return el.prototype.model_name;
+	return el.prototype.model_name;
 });
 
 
 var SongActionsRow = spv.inh(comd.PartsSwitcher, {
-  init: function(target) {
-    target.mo = target.map_parent;
-    pv.update(target, 'active_part', false);
-    //target.app = mo.app;
-    target.inited_parts = {};
-  }
+	init: function(target) {
+		target.mo = target.map_parent;
+		pv.update(target, 'active_part', false);
+		//target.app = mo.app;
+		target.inited_parts = {};
+	}
 }, {
   "+states": {
     "parent_show": ["compx", ['^mp_show']],
@@ -152,29 +152,29 @@ var SongActionsRow = spv.inh(comd.PartsSwitcher, {
         return fac && fac[0]/fac[1];
       }
     ]
-  },
+	},
 
-  sub_page: parts_storage,
-  'nest_posb-context_parts': constrs_names,
+	sub_page: parts_storage,
+	'nest_posb-context_parts': constrs_names,
 
-  'stch-parent_show': function (target, state) {
-    if (!state) {
-      target.hideAll();
-    }
-  },
+	'stch-parent_show': function (target, state) {
+		if (!state) {
+			target.hideAll();
+		}
+	},
 
-  sendVolume: function(vol) {
-    this.app.setSetting('volume', vol);
-  },
+	sendVolume: function(vol) {
+		this.app.setSetting('volume', vol);
+	},
 
-  setVolume: function(fac) {
-    if (!fac){
-      return;
-    }
-    this.sendVolume(fac);
-    this.map_parent.setVolume(fac);
+	setVolume: function(fac) {
+		if (!fac){
+			return;
+		}
+		this.sendVolume(fac);
+		this.map_parent.setVolume(fac);
 
-  }
+	}
 });
 
 return SongActionsRow;
