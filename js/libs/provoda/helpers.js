ae3929adfcc2170582c2312d8aabd267af3cebbf
Gleb Arestov <gr1b0k@gmail.com>
2019-10-30 19:10:17 +0300

    Add comment to improve

--- js/libs/provoda/helpers.js
+++ js/libs/provoda/helpers.js
@@ -184,6 +184,11 @@ return {
         throw new Error('no nesting_name');
       }
       /*
+        check perfomance:
+        1 create ID for nesting_name, nesting_space combo
+        2 save full concat result to  parent_view.child_location_cache
+      */
+      /*
       помогает определить есть ли у модели вьюха, ассоциированная с локацией - с родительской вьюхой (а также с гнездом внутри родительской вьюхи)
 
       */
