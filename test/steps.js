27228dc23159cb6b67707d8818e2b0df23043a36
Gleb Arestov <gr1b0k@gmail.com>
2019-11-16 11:04:59 +0300

    Add `steps` helper to init helper

--- test/steps.js
+++ test/steps.js
@@ -2,7 +2,7 @@ const waitFlow = require('./waitFlow')
 
 
 module.exports = app => {
-  const step = fn => () => waitFlow(app).then(() => fn())
+  const step = fn => () => waitFlow(app).then(app.inputFn(fn))
   const steps = fns => fns.reduce((result, fn) => result.then(step(fn)), waitFlow(app))
   return steps
 }
