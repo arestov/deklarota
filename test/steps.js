e257b2cb65383536073f9162352cd7d4b8bba5e9
Gleb Arestov <gr1b0k@gmail.com>
2019-11-16 11:43:25 +0300

    Fix steps for input

--- test/steps.js
+++ test/steps.js
@@ -2,7 +2,7 @@ const waitFlow = require('./waitFlow')
 
 
 module.exports = app => {
-  const step = fn => () => waitFlow(app).then(app.inputFn(fn))
+  const step = fn => () => waitFlow(app).then(app.inputFn(fn)).then(() => waitFlow(app))
   const steps = fns => fns.reduce((result, fn) => result.then(step(fn)), waitFlow(app))
   return steps
 }
