f9fb67363b0a790b019d50c86e6c7538195e410c
Gleb Arestov <gr1b0k@gmail.com>
2019-12-04 23:28:50 +0300

    Improve possible animations

---
+++ js/views/map_slice/anyDeeplyIncludedViews.js
@@ -0,0 +1,45 @@
+define(function() {
+'use strict';
+var getModelFromR = require('pv/v/getModelFromR')
+var getNesting = require('pv/getNesting')
+
+var matchParent = function(possible_parent, child) {
+  var cur = child;
+  while (cur.parent_view) {
+    if (cur.parent_view == possible_parent) {
+      return true
+    }
+
+    cur = cur.parent_view
+  }
+
+  return false;
+}
+
+return function(spyglass_view, current_bwlev_mdr, target_bwlev_mdr) {
+  // look if any views of md if deeply included in target_root
+  // to do this we will check if one of parent_view.parent_view[...] of mpx views is target_root
+  var current_bwlev = current_bwlev_mdr && getModelFromR(spyglass_view, current_bwlev_mdr)
+  if (!current_bwlev) {
+    return null
+  }
+
+  var cur_md = getNesting(current_bwlev, 'pioneer');
+  var bwlev_view = spyglass_view.getMapSliceView(current_bwlev, cur_md);
+
+  var target_bwlev = getModelFromR(spyglass_view, target_bwlev_mdr)
+  var target_md = getNesting(target_bwlev, 'pioneer');
+
+  var views = target_md.mpx.views
+  if (!views) {
+    return null;
+  }
+  for (var i = 0; i < views.length; i++) {
+    var cur = views[i]
+    if (matchParent(bwlev_view, cur)) {
+      return cur;
+    }
+
+  }
+}
+})
