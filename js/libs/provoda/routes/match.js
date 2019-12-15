52408763f9a0f0bf713be953c4b8d28b8b09df30
Gleb Arestov <gr1b0k@gmail.com>
2019-12-15 13:34:30 +0300

    New way to parse/match/stringify routes

---
+++ js/libs/provoda/routes/match.js
@@ -0,0 +1,51 @@
+define(function(require) {
+'use strict';
+
+var looksLikeWrappedJSON = require('./utils/looksLikeWrappedJSON')
+
+var tryParse = function(text) {
+  try {
+    var to_parse = text.slice(1, -1)
+    return JSON.parse(to_parse)
+  } catch (e) {
+    return text
+  }
+}
+
+
+var match = function(parsed, some_url) {
+  // 'tracks/[:artist:next_value],[:track]' + 'tracks/Mike,Play%20With%20You'
+  // => next_value: Mike, track: 'Play With You'
+  if (!some_url) {
+    return null
+  }
+
+  var matched = parsed.matcher.exec(some_url)
+  if (!matched) {
+    return null;
+  }
+
+  var result = {}
+
+  for (var i = 0; i < parsed.parts.length; i++) {
+    var cur = parsed.parts[i]
+    if (!cur.state) {
+      continue;
+    }
+    var raw_value = matched[cur.matching_group]
+    var decoded = decodeURIComponent(raw_value)
+
+    var unparsed = looksLikeWrappedJSON(decoded)
+      ? tryParse(decoded)
+      : decoded
+
+    var source = cur.state[0]
+
+    result[source] = unparsed
+  }
+
+  return result
+}
+
+return match
+})
---
+++ js/libs/provoda/routes/stringify.js
@@ -0,0 +1,44 @@
+define(function() {
+'use strict'
+var looksLikeWrappedJSON = require('./utils/looksLikeWrappedJSON')
+
+var statePartAsString = function(state_dcl, data) {
+  if (!state_dcl) {
+    return ''
+  }
+
+  var source = state_dcl[1]
+  var value = data[source]
+  if (value == null) {
+    return ''
+  }
+
+  if (typeof value == 'string') {
+    if (looksLikeWrappedJSON(value)) {
+      return encodeURIComponent(JSON.stringify(data[source]));
+    }
+
+    return encodeURIComponent(value);
+  }
+
+  return encodeURIComponent('"' + JSON.stringify(data[source]) + '"')
+}
+
+
+var toString = function(parsed, data) {
+  // 'tracks/[:artist:next_value],[:track]' +  {next_value: 'Mike', track: 'Play With You'}
+  // => 'tracks/Mike,Play%20With%20You'
+
+  var parts = parsed.parts
+  var toStrings = new Array(parts.length)
+  for (var i = 0; i < parts.length; i++) {
+    var cur = parts[i]
+    var result = (cur.prefix || '') + statePartAsString(cur.state, data)
+    toStrings[i] = result
+  }
+  return toStrings.join('')
+}
+
+return toString;
+
+})
