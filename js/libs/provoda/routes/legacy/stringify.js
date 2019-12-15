2c94331b5a09e2d8ef82eeef404566987a413b1c
Gleb Arestov <gr1b0k@gmail.com>
2019-12-15 13:00:02 +0300

    Move pathExecutor

---
+++ js/libs/provoda/routes/legacy/stringify.js
@@ -0,0 +1,34 @@
+define(function(require) {
+'use strict'
+
+var getValue = function (app, value) {
+  if (value) {
+    return app.encodeURLPart(value);
+  }
+
+  if (value === 0) {
+    return app.encodeURLPart(value);
+  }
+};
+
+var pathExecutor = function(getChunk) {
+  return function getPath(obj, app, arg1, arg2) {
+    if (obj.parsed.states) {
+      var full_path = '';
+      for (var i = 0; i < obj.parsed.clean_string_parts.length; i++) {
+        full_path += obj.parsed.clean_string_parts[i];
+        var cur_state = obj.parsed.states[i];
+        if (cur_state) {
+          var chunk = getChunk(cur_state, app, arg1, arg2);
+          full_path += getValue(app, chunk) || 'null';
+        }
+      }
+      return full_path;
+    }
+    return obj.full_usable_string;
+  };
+};
+
+return pathExecutor
+
+})
