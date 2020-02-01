a190ee08f46ec1c3597f1c9381c88dc52f72d520
Gleb Arestov <gr1b0k@gmail.com>
2020-02-01 16:33:50 +0300

    Batch updates
    Crate `update` list message

--- js/libs/provoda/sync_sender.js
+++ js/libs/provoda/sync_sender.js
@@ -8,6 +8,44 @@ var SyncSender = function() {
   this.sockets = {};
   this.streams_list = [];
   this.sockets_m_index ={};
+  this.batched_by_id = {}
+
+  this.st_counter = 0
+  this.batched = []
+  this.schedule_timer = null
+  this.has_nothing = true
+
+  var self = this
+
+  // 1 send operation cost is about 1ms-12ms. so 100 operations can take up to 100ms-1000ms
+  // lets butch operations. 60fps is 16ms per frame. lets batch by 16ms.
+
+  this.flush = function() {
+    self.has_nothing = true
+
+    for (var i = 0; i < self.streams_list.length; i++) {
+      var cur = self.streams_list[i];
+      var list = self.batched_by_id[cur.id]
+      if (!list.length) {
+        continue
+      }
+
+      self.has_nothing = false
+
+      self.batched_by_id[cur.id] = null // protect from race conditions
+
+      cur.send(list)
+
+      list.length = null
+      self.batched_by_id[cur.id] = list
+    }
+
+    if (self.has_nothing) {
+      clearInterval(self.schedule_timer)
+      self.schedule_timer = null
+    }
+  }
+
 };
 
 var toTransferableNestings = function(value) {
@@ -45,16 +83,26 @@ var toTransferableNestings = function(value) {
 }
 
 SyncSender.prototype = {
+  schedule: function() {
+    if (this.schedule_timer !== null) {
+      return
+    }
+
+    this.schedule_timer = setInterval(this.flush, 16)
+  },
   removeSyncStream: function(stream) {
     if (!this.sockets[stream.id]) {
       return;
     }
     this.sockets_m_index[stream.id] = null;
+    this.batched_by_id[stream.id] = null;
     this.sockets[stream.id] = null;
     this.streams_list = spv.findAndRemoveItem(this.streams_list, stream);
   },
   addSyncStream: function(start_md, stream) {
     this.sockets_m_index[stream.id] = {};
+    this.batched_by_id[stream.id] = [];
+
     this.sockets[stream.id] = stream;
     this.streams_list.push(stream);
 
@@ -66,6 +114,7 @@ SyncSender.prototype = {
     //var struc;
     var parsed_value = toTransferableNestings(value)
 
+
     for (var i = 0; i < this.streams_list.length; i++) {
       var cur = this.streams_list[i];
       var index = this.sockets_m_index[cur.id];
@@ -73,10 +122,15 @@ SyncSender.prototype = {
         continue;
       }
 
-      var struc = md.toSimpleStructure(index);
-      cur.changeCollection(md._provoda_id, struc, nesname, parsed_value);
+      var struc = toSimpleStructure(this.sockets_m_index[cur.id]);
 
+      var list = this.batched_by_id[cur.id]
+      list.push(
+        ['nest-ch', md._provoda_id, struc, nesname, parsed_value]
+      )
     }
+
+    this.schedule()
   },
   pushStates: function(md, states_raw) {
   //	var struc;
@@ -84,11 +138,18 @@ SyncSender.prototype = {
 
     for (var i = 0; i < this.streams_list.length; i++) {
       var cur = this.streams_list[i];
-      if (this.sockets_m_index[cur.id][md._provoda_id]) {
-
-        cur.updateStates(md._provoda_id, states);
+      var index = this.sockets_m_index[cur.id];
+      if (!index[md._provoda_id]){
+        continue;
       }
+
+      var list = this.batched_by_id[cur.id]
+      list.push(
+        ['state-ch', md._provoda_id, states]
+      )
     }
+
+    this.schedule()
   }
 };
 return SyncSender;
