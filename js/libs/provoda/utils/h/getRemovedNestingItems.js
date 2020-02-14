b5924cbcb59a3245110558b42182af712690a3a0
Gleb Arestov <gr1b0k@gmail.com>
2020-02-14 10:52:06 +0300

    Move getRemovedNestingItems

---
+++ js/libs/provoda/utils/h/getRemovedNestingItems.js
@@ -0,0 +1,23 @@
+define(function() {
+'use strict'
+return function(array, old_value) {
+  var removed;
+  if (Array.isArray(old_value)){
+    if (!array){
+      removed = old_value.slice(0);
+    } else {
+      removed = [];
+      for (var i = 0; i < old_value.length; i++) {
+        var cur = old_value[i];
+        if (array.indexOf(cur) == -1){
+          removed.push(cur);
+        }
+      }
+    }
+    //console.log(removed);
+  } else if (old_value && array != old_value) {
+    removed = [old_value];
+  }
+  return removed;
+}
+})
