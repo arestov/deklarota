762974601565d82caa2e7e705a6dece62dbd39a6
Gleb Arestov <gr1b0k@gmail.com>
2020-02-23 21:18:55 +0200

    Move zip/nest-watch

--- js/libs/provoda/nest-watch/getStateWriter.js
+++ js/libs/provoda/nest-watch/getStateWriter.js
@@ -2,52 +2,12 @@ define(function (require) {
 'use strict';
 
 var spv = require('spv');
-var pvState = require('../utils/state');
+var zip_fns = require('../utils/zip/nest-watch')
 var updateProxy = require('../updateProxy');
 var pvUpdate = updateProxy.update;
 var standart = require('./standartNWH');
 
 
-
-var stateOf = spv.memorize(function(state_name) {
-  return function(md) {
-    return pvState(md, state_name);
-  };
-});
-
-var stateG = function(callback) {
-  return function(state_name) {
-    return callback(stateOf(state_name));
-  };
-};
-
-var toZipFunc = function(toValue) {
-  return spv.memorize(stateG(toValue));
-};
-
-var map = toZipFunc(function(state) {
-  return function(array) {
-    return array && array.map(state);
-  };
-});
-
-var some = toZipFunc(function(state) {
-  return function(array) {
-    return array.some(state);
-  };
-});
-
-var every = toZipFunc(function(state) {
-  return function(array) {
-    return array.every(state);
-  };
-});
-
-var one = toZipFunc(function(state) {
-  return function(array) {
-    return array[0] && state(array[0]);
-  };
-});
 
 var arrayClone = function(array) {
   if (Array.isArray(array)) {
@@ -63,24 +23,12 @@ var getZipFunc = spv.memorize(function(state_name, zip_name) {
     return arrayClone;
   }
 
-  if (!zip_name) {
-    return map(state_name);
-  } else {
-    switch (zip_name) {
-      case 'one': {
-        return one(state_name);
-      }
-      case 'some': {
-        return some(state_name);
-      }
-      case 'every': {
-        return every(state_name);
-      }
-      default: {
-        throw new Error('unknow zip func ' + zip_name);
-      }
-    }
+  var createZipFn = zip_fns[zip_name || 'all']
+  if (!createZipFn) {
+    throw new Error('unknow zip func ' + zip_name);
   }
+
+  return createZipFn(state_name)
 }, function(state_name, zip_name) {
   return (state_name || "") + '-' + (zip_name || "");
 });
---
+++ js/libs/provoda/utils/zip/nest-watch.js
@@ -0,0 +1,62 @@
+define(function() {
+'use strict'
+var spv = require('spv')
+var pvState = require('../state');
+
+var stateOf = spv.memorize(function(state_name) {
+  return function(md) {
+    return pvState(md, state_name);
+  };
+});
+
+var stateG = function(callback) {
+  return function(state_name) {
+    return callback(stateOf(state_name));
+  };
+};
+
+var toZipFunc = function(toValue) {
+  return spv.memorize(stateG(toValue));
+};
+
+var map = toZipFunc(function(state) {
+  return function(array) {
+    return array && array.map(state);
+  };
+});
+
+
+var some = toZipFunc(function(state) {
+  return function(array) {
+    return array.some(state);
+  };
+});
+
+var every = toZipFunc(function(state) {
+  return function(array) {
+    return array.every(state);
+  };
+});
+
+var one = toZipFunc(function(state) {
+  return function(array) {
+    return array[0] && state(array[0]);
+  };
+});
+
+return {
+  'all': function(state_name) {
+    return map(state_name)
+  },
+  'one': function(state_name) {
+    return one(state_name);
+  },
+  'some': function(state_name) {
+    return some(state_name);
+  },
+  'every': function(state_name) {
+    return every(state_name)
+  }
+}
+
+})
