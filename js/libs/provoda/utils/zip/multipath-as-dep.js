b2d4140788475f1bebe7f7a9bb2324d20a096c8e
Gleb Arestov <gr1b0k@gmail.com>
2020-02-23 20:49:36 +0200

    Allow use zip in getValues

---
+++ js/libs/provoda/utils/zip/multipath-as-dep.js
@@ -0,0 +1,24 @@
+define(function(require) {
+'use strict'
+var isMeaningfulValue = require('../isMeaningfulValue')
+
+var zip_fns = {
+  'one': function(list) {
+    return list && list[0]
+  },
+  'every': function(list) {
+    return list && list.every(isMeaningfulValue)
+  },
+  'some': function(list) {
+    return list && list.some(isMeaningfulValue)
+  },
+  'find': function(list) {
+    return list && list.find(isMeaningfulValue)
+  },
+  'all': function(list) {
+    return list
+  }
+}
+
+return zip_fns
+})
