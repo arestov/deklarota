67b14a3884e5282898a482de9209caf690197043
Gleb Arestov <gr1b0k@gmail.com>
2020-02-23 10:59:44 +0300

    Refactor getValues

--- js/libs/provoda/utils/multiPath/getValues.js
+++ js/libs/provoda/utils/multiPath/getValues.js
@@ -16,11 +16,16 @@ var getValue = function(md, multi_path) {
     }
   }
 
-  if (multi_path.as_string != '<<<<') {
-    console.warn('is it good idea!?', 'should not we throw error here?')
-  }
 
   return md
+}
+
+var getOne = function (items) {
+  if (!Array.isArray(items)) {
+    return items
+  }
+
+  return items && items[0]
 }
 
 return function(models, multi_path) {
@@ -28,22 +33,47 @@ return function(models, multi_path) {
     throw new Error('implenent me')
   }
 
-  if (!Array.isArray(models)) {
-    return getValue(models, multi_path)
+  switch (multi_path.result_type) {
+    case "state": {
+      if (!Array.isArray(models)) {
+        return getValue(models, multi_path)
+      }
+
+      var result = new Array(models.length)
+      for (var i = 0; i < models.length; i++) {
+        result[i] = getValue(models[i], multi_path)
+      }
+
+      return result
+    }
+
+    case "nesting": {
+
+      // results is always array here
+      var result = []
+      for (var i = 0; i < models.length; i++) {
+        var cur = getValue(models[i], multi_path)
+        if (!cur) {continue}
+
+        result.push(cur)
+      }
+
+      result = Array.prototype.concat.apply([], result)
+      return result
+    }
   }
 
-  var result = []
-  for (var i = 0; i < models.length; i++) {
-    var cur = getValue(models[i], multi_path)
-    if (!cur) {continue}
-    result.push(cur)
+  if (multi_path.as_string != '<<<<') {
+    /*
+      ok to get
+      self
+      parent/root
+      path
+    */
+    // console.warn('is it good idea!?', 'should not we throw error here?')
   }
 
-  if (multi_path.result_type !== 'nesting') {
-    return result
-  }
-
-  return Array.prototype.concat.apply([], result)
+  return models
 
 };
 })
