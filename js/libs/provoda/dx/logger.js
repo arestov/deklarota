e7d3d003a8236ee09d5dba0a7734be2edea468b7
Gleb Arestov <gr1b0k@gmail.com>
2020-03-09 08:00:42 +0300

    Change logger api

--- js/libs/provoda/dx/logger.js
+++ js/libs/provoda/dx/logger.js
@@ -1,11 +1,37 @@
 define(function() {
 'use strict'
+
+var checkState = function(owner, state_name) {
+  if (!owner._highway.logger.checkState) {
+    return true
+  }
+
+  return owner._highway.logger.checkState(state_name, owner)
+}
+
 var logStates = function(owner, dubl) {
   if (!owner._highway.logger) {
     return
   }
 
-  owner._highway.logger.pushStates(owner, dubl);
+  var list = dubl
+  var changes = []
+  for (var i = 0; i < list.length; i += 3) {
+    var name = list[i + 1]
+    if (!checkState(owner, name)) {
+      continue
+    }
+
+    var newValue = list[i + 2]
+
+    changes.push([name, newValue])
+  }
+
+  if (!changes.length) {
+    return
+  }
+
+  owner._highway.logger.pushStates(owner, changes);
 }
 
 var logNesting = function(owner, collection_name, array, old_value, removed) {
