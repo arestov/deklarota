31d60ca5c877fcb096af53fe60da878335d791f0
Gleb Arestov <gr1b0k@gmail.com>
2020-03-28 14:49:18 +0300

    Support modern paths in attr compx

--- js/libs/provoda/utils/getEncodedState.js
+++ js/libs/provoda/utils/getEncodedState.js
@@ -5,9 +5,37 @@ var NestWatch = require('../nest-watch/NestWatch');
 var getStateWriter = require('../nest-watch/getStateWriter');
 var getParsedState = require('./getParsedState')
 var toMultiPath = require('./NestingSourceDr/toMultiPath')
+var parseMultiPath = require('./multiPath/parse')
+var asString = require('./multiPath/asString')
+
+var fromMultiPath = getParsedState.fromMultiPath
+
+var getMultiPath = function(full_name) {
+  if (full_name && full_name.charAt(0) == '<') {
+    return parseMultiPath(full_name)
+  }
+}
+
+var ensureResult = function(full_name) {
+  var multi_path = getMultiPath(full_name)
+  if (!multi_path) {
+    return null
+  }
+
+  return fromMultiPath(multi_path, asString(multi_path))
+}
 
 var getEncodedState = spv.memorize(function getEncodedState(state_name) {
-  var result = getParsedState(state_name)
+
+
+  var result1 = getParsedState(state_name)
+  // if (result1) { // uncomment to help migrate
+  //   var nice = fromLegacy(state_name)
+  //   var best = asString(nice)
+  //   console.warn('replace ' + state_name + ' by ' + best)
+  // }
+
+  var result = result1 || ensureResult(state_name)
 
   if (!result) {
     return null
