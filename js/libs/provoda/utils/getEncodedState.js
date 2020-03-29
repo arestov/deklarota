17735631201a16e7840f153d36905b6735eb39ba
Gleb Arestov <gr1b0k@gmail.com>
2020-03-29 12:43:25 +0300

    Move code

--- js/libs/provoda/utils/getEncodedState.js
+++ js/libs/provoda/utils/getEncodedState.js
@@ -5,25 +5,9 @@ var NestWatch = require('../nest-watch/NestWatch');
 var getStateWriter = require('../nest-watch/getStateWriter');
 var getParsedState = require('./getParsedState')
 var toMultiPath = require('./NestingSourceDr/toMultiPath')
-var parseMultiPath = require('./multiPath/parse')
-var asString = require('./multiPath/asString')
-
-var fromMultiPath = getParsedState.fromMultiPath
-
-var getMultiPath = function(full_name) {
-  if (full_name && full_name.charAt(0) == '<') {
-    return parseMultiPath(full_name)
-  }
-}
-
-var ensureResult = function(full_name) {
-  var multi_path = getMultiPath(full_name)
-  if (!multi_path) {
-    return null
-  }
-
-  return fromMultiPath(multi_path, asString(multi_path))
-}
+// var asString = require('./multiPath/asString')
+// var fromLegacy = require('./multiPath/fromLegacy')
+var modernAsLegacyParsed = require('./modernAsLegacyParsed')
 
 var getEncodedState = spv.memorize(function getEncodedState(state_name) {
 
@@ -35,7 +19,7 @@ var getEncodedState = spv.memorize(function getEncodedState(state_name) {
   //   console.warn('replace ' + state_name + ' by ' + best)
   // }
 
-  var result = result1 || ensureResult(state_name)
+  var result = result1 || modernAsLegacyParsed(state_name)
 
   if (!result) {
     return null
---
+++ js/libs/provoda/utils/modernAsLegacyParsed.js
@@ -0,0 +1,25 @@
+define(function(require) {
+'use strict';
+var getParsedState = require('./getParsedState')
+var asString = require('./multiPath/asString')
+var parseMultiPath = require('./multiPath/parse')
+
+var fromMultiPath = getParsedState.fromMultiPath
+
+var getMultiPath = function(full_name) {
+  if (full_name && full_name.charAt(0) == '<') {
+    return parseMultiPath(full_name)
+  }
+}
+
+var ensureResult = function(full_name) {
+  var multi_path = getMultiPath(full_name)
+  if (!multi_path) {
+    return null
+  }
+
+  return fromMultiPath(multi_path, asString(multi_path))
+}
+
+return ensureResult
+})
