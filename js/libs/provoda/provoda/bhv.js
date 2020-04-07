b1d8979f6eebaa6a4c3d8f634e01503b00953dc5
Gleb Arestov <gr1b0k@gmail.com>
2020-04-07 20:58:57 +0300

    Improve using extends

--- js/libs/provoda/provoda/bhv.js
+++ js/libs/provoda/provoda/bhv.js
@@ -5,7 +5,15 @@ var Model = require('../Model');
 
 return function behavior(declr, declr_extend_from, named) {
   var behaviorFrom = declr.extends || declr_extend_from || Model;
-  delete declr.extends
+  if (declr.extends && declr_extend_from) {
+    throw new Error('choose one: `extends` param or Model arg')
+  }
+
+  if (declr.extends) {
+    declr = spv.cloneObj({}, declr)
+    delete declr.extends
+  }
+
   if (typeof named == 'object' || !declr.init) {
     return spv.inh(behaviorFrom, {
       naming: named && named.naming,
