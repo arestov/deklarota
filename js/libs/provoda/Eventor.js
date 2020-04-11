44e511d3bb678a44454ef0f341476d71d7369f96
Gleb Arestov <gr1b0k@gmail.com>
2020-04-11 20:04:54 +0300

    Fix require hndMotivationWrappper

--- js/libs/provoda/Eventor.js
+++ js/libs/provoda/Eventor.js
@@ -3,7 +3,7 @@ define(function(require) {
 
 var spv = require('spv');
 var FastEventor = require('./FastEventor/index');
-var hp = require('./helpers');
+var hndMotivationWrappper = require('./helpers/hndMotivationWrappper');
 
 
 var Eventor = spv.inh(function() {}, {
@@ -46,11 +46,11 @@ var Eventor = spv.inh(function() {}, {
       return result;
     },
     nextLocalTick: function(fn, args, use_current_motivator, finup) {
-      return this._getCallsFlow().pushToFlow(fn, this, args, false, hp.oop_ext.hndMotivationWrappper, this, use_current_motivator && this.current_motivator, finup);
+      return this._getCallsFlow().pushToFlow(fn, this, args, false, hndMotivationWrappper, this, use_current_motivator && this.current_motivator, finup);
     },
     nextTick: function(fn, args, use_current_motivator, initiator) {
       return this._calls_flow.pushToFlow(
-        fn, this, args, !args && this, hp.oop_ext.hndMotivationWrappper, this, use_current_motivator && this.current_motivator, false,
+        fn, this, args, !args && this, hndMotivationWrappper, this, use_current_motivator && this.current_motivator, false,
         initiator, fn.init_end
       );
     },
