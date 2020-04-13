6f1580da43376745234afd80d1d0cbab044a9bd8
Gleb Arestov <gr1b0k@gmail.com>
2020-04-13 20:41:26 +0300

    Move css env detection

--- js/views/modules/view_serv.js
+++ js/views/modules/view_serv.js
@@ -4,50 +4,11 @@ var spv = require('spv');
 var env = require('env');
 var localizer = require('localizer');
 var $ = require('cash-dom');
+var css = require('./view_serv/css')
 
-var serv = {};
+var serv = {css: css};
 
-(function() {
-  var css = {};
 
-  var dom_style_obj = window.document.createElement('div').style;
-  var has_transform_prop;
-  var has_transition_prop;
-
-  var transition_props = {
-    //https://github.com/ai/transition-events/blob/master/lib/transition-events.js
-    // Webkit must be on bottom, because Opera try to use webkit
-    // prefix.
-    'transition':		'transitionend',
-    'OTransition':		'oTransitionEnd',
-    'WebkitTransition':	'webkitTransitionEnd',
-    'MozTransition':	'transitionend'
-  };
-
-  for ( var prop in transition_props ) {
-    if (prop in dom_style_obj){
-      has_transition_prop = transition_props[prop];
-      break;
-    }
-  }
-
-  ['transform', '-o-transform', '-webkit-transform', '-moz-transform'].forEach(function(el) {
-    if (!has_transform_prop && el in dom_style_obj){
-      has_transform_prop = el;
-    }
-  });
-
-  if (has_transition_prop){
-    css.transition = has_transition_prop;
-  }
-
-  if (has_transform_prop){
-    css.transform = has_transform_prop;
-  }
-
-  serv.css = css;
-
-})();
 
 var addClass = function(old_c, cl){
   var add_c = cl.split(' ');
