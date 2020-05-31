396150556ddf5bfff0b09c08390a2cc20a21224e
Gleb Arestov <gr1b0k@gmail.com>
2020-05-31 23:37:12 +0300

    Move ensurePublicAttrs

--- js/libs/provoda/Model/toSimpleStructure.js
+++ js/libs/provoda/Model/toSimpleStructure.js
@@ -1,9 +1,8 @@
 define(function(require) {
 'use strict';
 var spv = require('spv')
+var ensurePublicAttrs = require('./ensurePublicAttrs')
 var cloneObj = spv.cloneObj
-var isPrivate = require('./isPrivateState')
-var replaceModelInState = require('./replaceModelInState')
 
 var checkModel = function(md, models_index, local_index, all_for_parse) {
   var cur_id = md._provoda_id;
@@ -47,26 +46,6 @@ var handleNesting = function(cur, models_index, local_index, all_for_parse) {
   return array
 }
 
-var ensurePublicAttrs = function(cur_md) {
-  if (cur_md._prepared_public_attrs_snapshot) {
-    return cur_md._prepared_public_attrs_snapshot
-  }
-
-  var result = cloneObj({}, cur_md.states)
-
-  for (var state_name in result){
-    var state = result[state_name];
-    if (isPrivate(state_name)) {
-      delete result[state_name];
-      continue;
-    }
-    result[state_name] = replaceModelInState(state)
-  }
-
-
-  cur_md._prepared_public_attrs_snapshot = result
-  return result
-}
 
 var iterate = function( models_index, all_for_parse, local_index, big_result) {
 
