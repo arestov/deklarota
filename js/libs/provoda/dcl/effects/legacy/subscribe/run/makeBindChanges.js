57562bcd0d8ee877fbffbcf727c212e013c5d347
Gleb Arestov <gr1b0k@gmail.com>
2020-05-31 19:54:16 +0300

    Refactor subscribe effect handlers

--- js/libs/provoda/dcl/effects/legacy/subscribe/run/makeBindChanges.js
+++ js/libs/provoda/dcl/effects/legacy/subscribe/run/makeBindChanges.js
@@ -2,36 +2,41 @@ define(function() {
 'use strict'
 // state_name в данном контексте просто key (за исключенимем момента когда нужно вызвать getStateUpdater)
 
-var getStateUpdater = function(em, state_name) {
-  if (!em._state_updaters) {
-    em._state_updaters = {};
-  }
-  if (!em._state_updaters.hasOwnProperty(state_name)) {
-    em._state_updaters[state_name] = em.inputFn(function(value) {
-      em.updateState(state_name, value);
-    });
-  }
-  return em._state_updaters[state_name];
-};
+var ensureHandler = function(fn) {
+  return function(em, dcl, exactPart) {
+    if (!em._highway._subscribe_effect_handlers) {
+      em._highway._subscribe_effect_handlers  = {}
+    }
 
-var getPassDispatcher = function(em, pass_name) {
-  if (!em._pass_dispatchers) {
-    em._pass_dispatchers = {};
-  }
+    var store = em._highway._subscribe_effect_handlers
 
-  if (!em._pass_dispatchers.hasOwnProperty(pass_name)) {
-    em._pass_dispatchers[pass_name] = em.inputFn(function(data) {
-      this.__act(this, pass_name, data)
+    var key = em.getInstanceKey() + '-' + dcl.id
+
+    if (store[key]) {
+      return store[key]
+    }
+
+    store[key] = em.inputFn(function(value) {
+      fn(this, exactPart, value)
     });
+
+    return store[key]
   }
-  return em._pass_dispatchers[pass_name];
 }
+
+var getStateUpdater = ensureHandler(function(em, state_name, value) {
+  em.updateState(state_name, value);
+})
+
+var getPassDispatcher = ensureHandler(function(em, pass_name, data) {
+  em.__act(em, pass_name, data);
+})
 
 var getHandler = function(self, dcl) {
   if (dcl.pass_name) {
-    return getPassDispatcher(self, dcl.pass_name)
+    return getPassDispatcher(self, dcl, dcl.pass_name)
   }
-  return getStateUpdater(self, dcl.state_name);
+  return getStateUpdater(self, dcl, dcl.state_name);
 }
 
 
