dcc27f44218d3871071814ace6c11bbf84794053
Gleb Arestov <gr1b0k@gmail.com>
2020-06-20 20:07:27 +0300

    Move code for `lgh_sch-`

---
+++ js/libs/provoda/internal_events/light_attr_change/getAttrByName.js
@@ -0,0 +1,6 @@
+define(function (require) {
+'use strict';
+var spv = require('spv');
+
+return spv.getDeprefixFunc('lgh_sch-')
+})
---
+++ js/libs/provoda/internal_events/light_attr_change/getNameByAttr.js
@@ -0,0 +1,6 @@
+define(function (require) {
+'use strict';
+var spv = require('spv');
+
+return spv.getPrefixingFunc('lgh_sch-')
+})
