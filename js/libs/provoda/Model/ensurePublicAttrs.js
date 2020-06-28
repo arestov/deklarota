e7a7ce0ad06cb2ecf1db824df5dcae17b10c3e47
Gleb Arestov <gr1b0k@gmail.com>
2020-06-28 12:30:55 +0300

    Create assignPublicAttrs

--- js/libs/provoda/Model/ensurePublicAttrs.js
+++ js/libs/provoda/Model/ensurePublicAttrs.js
@@ -3,13 +3,8 @@ define(function(require) {
 var isPrivate = require('./isPrivateState')
 var replaceModelInState = require('./replaceModelInState')
 
-
-var ensurePublicAttrs = function(cur_md) {
-  if (cur_md._prepared_public_attrs_snapshot) {
-    return cur_md._prepared_public_attrs_snapshot
-  }
-
-  var result = {}
+var assignPublicAttrs = function(cur_md, target) {
+  var result = target
 
   for (var state_name in cur_md.states){
     var state = cur_md.states[state_name];
@@ -19,6 +14,15 @@ var ensurePublicAttrs = function(cur_md) {
     result[state_name] = replaceModelInState(state)
   }
 
+  return result
+}
+
+var ensurePublicAttrs = function(cur_md) {
+  if (cur_md._prepared_public_attrs_snapshot) {
+    return cur_md._prepared_public_attrs_snapshot
+  }
+
+  var result = assignPublicAttrs(cur_md, {})
 
   cur_md._prepared_public_attrs_snapshot = result
   return result
