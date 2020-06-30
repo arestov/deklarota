f5d6596cfab908db065a95285750b4955ec62164
Gleb Arestov <gr1b0k@gmail.com>
2020-06-30 10:11:12 +0300

    Add resetRequestedState method

--- js/libs/provoda/FastEventor/requesting.js
+++ js/libs/provoda/FastEventor/requesting.js
@@ -6,6 +6,7 @@ var requestNesting = require('./requestNesting')
 
 return {
   requestState: requestState,
+  resetRequestedState: requestState.resetRequestedState,
   requestNesting: requestNesting,
 };
 
