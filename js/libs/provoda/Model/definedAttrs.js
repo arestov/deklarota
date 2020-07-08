d94d3b2addde060334ce7dd52e5ddc82fd00ce58
Gleb Arestov <gr1b0k@gmail.com>
2020-07-08 11:24:45 +0300

    Define main attrs

--- js/libs/provoda/Model/definedAttrs.js
+++ js/libs/provoda/Model/definedAttrs.js
@@ -9,6 +9,27 @@ return function(self) {
 
   var result = [{name: '$meta$inited', type: 'bool'}]
 
+  // Default attrs
+  if (self.__default_attrs) {
+    for (var attr in self.__default_attrs) {
+      if (!self.__default_attrs.hasOwnProperty(attr)) {
+        continue
+      }
+      result.push({name: attr})
+    }
+  }
+
+  // Compx attrs
+  if (self.full_comlxs_list) {
+    for (var i = 0; i < self.full_comlxs_list.length; i++) {
+      var cur = self.full_comlxs_list[i]
+      var isFullBool = cur.fn === Boolean && !cur.require_marks.length
+      result.push({name: cur.name, type: isFullBool ? 'bool' : null})
+    }
+  }
+
+
+  // Meta attrs of requests
   if (self._states_reqs_list) {
     for (var i = 0; i < self._states_reqs_list.length; i++) {
       var cur = self._states_reqs_list[i]
@@ -16,11 +37,22 @@ return function(self) {
     }
   }
 
+  // Meta attrs of apis
   if (self.__defined_api_attrs_bool) {
     push.apply(result, self.__defined_api_attrs_bool)
   }
 
   self.__defined_attrs_bool = result
+
+  // DONT NOT PREDEFINE things that could be bool (like $routed, $length)
+
+  // for (var i = 0; i < self.full_comlxs_list.length; i++) {
+  //   var cur = self.full_comlxs_list[i]
+  //   for (var jj = 0; jj < cur.watch_list.length; jj++) {
+  //     result.push({name: cur.name, type: cur.fn === Boolean ? 'bool' : null})
+  //   }
+  // }
+
   return result
 
 
