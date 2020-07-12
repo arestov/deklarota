6c3cb575fbe442c65842572e05b7943de8415205
Gleb Arestov <gr1b0k@gmail.com>
2020-07-12 18:59:39 +0300

    Move createModern

---
+++ js/libs/provoda/dcl/routes/createModern.js
@@ -0,0 +1,62 @@
+define(function() {
+'use strict'
+var matchRoute = require('../../routes/match')
+var get_constr = require('../../structure/get_constr');
+var getNestingConstr = get_constr.getNestingConstr
+var allStates = require('./allStates')
+
+var createStates = function (Constr, sp_name, extra_states) {
+  var has_compx = Constr.prototype.hasComplexStateFn('url_part')
+  if (has_compx) {
+    return allStates(null, extra_states)
+  }
+
+  return allStates({
+    url_part: '/' + sp_name
+  }, extra_states)
+}
+
+function selectModern(self, sp_name) {
+  if (self.__routes_matchers_defs == null) {
+    return
+  }
+
+  for (var i = 0; i < self.__routes_matchers_defs.length; i++) {
+    var cur = self.__routes_matchers_defs[i];
+    var matched = matchRoute(cur.route, sp_name)
+    if (!matched) {
+      continue;
+    }
+
+    var Constr = getNestingConstr(self.app, self, cur.dest)
+
+    return {matched: matched, routedcl: cur, Constr: Constr}
+  }
+}
+
+function createModern(self, sp_name, extra_states) {
+  var selected = selectModern(self, sp_name)
+  if (!selected) {
+    return
+  }
+
+  var Constr = selected.Constr
+
+  var created = self.initSi(Constr, {
+    by: 'routePathByModels',
+    init_version: 2,
+    states: createStates(Constr, sp_name, extra_states),
+    head: selected.matched,
+  });
+
+  var nesting_name = selected.routedcl.dest
+  var cur_list = self.getNesting(selected.routedcl.dest)
+  var new_list = cur_list ? cur_list.slice(0) : []
+  new_list.push(created)
+  self.updateNesting(nesting_name, new_list)
+
+  return created
+}
+
+return createModern
+})
--- js/libs/provoda/routePathByModels.js
+++ js/libs/provoda/routePathByModels.js
@@ -1,12 +1,10 @@
 define(function (require) {
 'use strict';
 var spv = require('spv');
-var matchRoute = require('./routes/match')
-var get_constr = require('./structure/get_constr');
 var allStates = require('./dcl/routes/allStates')
 var getModernPage = require('./dcl/routes/getByName')
+var createModern = require('./dcl/routes/createModern')
 
-var getNestingConstr = get_constr.getNestingConstr
 
 var cloneObj = spv.cloneObj;
 var getSPI = getterSPI();
@@ -151,58 +149,6 @@ function getSPOpts(md, sp_name, slashed, byType) {
 };
 
 
-  var createStates = function (Constr, sp_name, extra_states) {
-    var has_compx = Constr.prototype.hasComplexStateFn('url_part')
-    if (has_compx) {
-      return allStates(null, extra_states)
-    }
-
-    return allStates({
-      url_part: '/' + sp_name
-    }, extra_states)
-  }
-
-  function selectModern(self, sp_name) {
-    if (self.__routes_matchers_defs == null) {
-      return
-    }
-
-    for (var i = 0; i < self.__routes_matchers_defs.length; i++) {
-      var cur = self.__routes_matchers_defs[i];
-      var matched = matchRoute(cur.route, sp_name)
-      if (!matched) {
-        continue;
-      }
-
-      var Constr = getNestingConstr(self.app, self, cur.dest)
-
-      return {matched: matched, routedcl: cur, Constr: Constr}
-    }
-  }
-
-  function createModern(self, sp_name, extra_states) {
-    var selected = selectModern(self, sp_name)
-    if (!selected) {
-      return
-    }
-
-    var Constr = selected.Constr
-
-    var created = self.initSi(Constr, {
-      by: 'routePathByModels',
-      init_version: 2,
-      states: createStates(Constr, sp_name, extra_states),
-      head: selected.matched,
-    });
-
-    var nesting_name = selected.routedcl.dest
-    var cur_list = self.getNesting(selected.routedcl.dest)
-    var new_list = cur_list ? cur_list.slice(0) : []
-    new_list.push(created)
-    self.updateNesting(nesting_name, new_list)
-
-    return created
-  }
 
 function getterSPI(){
   var init = function(parent, target, data) {
