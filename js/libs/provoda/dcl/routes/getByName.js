e9e73ec09188e39cf5e07850ec33592500fc7169
Gleb Arestov <gr1b0k@gmail.com>
2020-07-12 19:28:06 +0300

    Make getByName works in lazy way

--- js/libs/provoda/dcl/routes/getByName.js
+++ js/libs/provoda/dcl/routes/getByName.js
@@ -1,9 +1,32 @@
-define(function() {
+define(function(require) {
 'use strict'
+var spv = require('spv')
+var cloneObj = spv.cloneObj
+
 return function(self, sp_name) {
-  if (self.__modern_subpages == null) {
+  if (self.__routes_matchers_defs == null) {
     return null
   }
+
+  if (self.__modern_subpages_valid) {
+    return self.__modern_subpages[sp_name]
+  }
+
+  self.__modern_subpages_valid = true
+
+  var result = {}
+
+  for (var i = self.__routes_matchers_runs.length - 1; i >= 0; i--) {
+    var cur = self.__routes_matchers_runs[i]
+    if (!cur.matched) {
+      continue
+    }
+
+    cloneObj(result, cur.matched)
+  }
+
+  self.__modern_subpages = result
+
   return self.__modern_subpages[sp_name]
 }
 })
--- js/libs/provoda/dcl/routes/run.js
+++ js/libs/provoda/dcl/routes/run.js
@@ -1,26 +1,11 @@
-define(function (require) {
+define(function () {
 'use strict'
-var spv = require('spv')
-var cloneObj = spv.cloneObj
-var utils_simple = require('../../utils/simple');
-var wipeObj = utils_simple.wipeObj
 
 return function(runner) {
   var self = runner.md
-  var result = wipeObj(self.__modern_subpages)
+
+  self.__modern_subpages_valid = false
   self.__modern_subpages = null
 
-  for (var i = self.__routes_matchers_runs.length - 1; i >= 0; i--) {
-    var cur = self.__routes_matchers_runs[i]
-    if (!cur.matched) {
-      continue
-    }
-
-    result = result || {}
-    cloneObj(result, cur.matched)
-  }
-
-
-  self.__modern_subpages = result
 }
 })
