e313149758a6e0e57059b09ed5ddc7a00e553946
Gleb Arestov <gr1b0k@gmail.com>
2020-07-16 12:06:27 +0300

    Improve stateGetter

--- js/libs/provoda/utils/stateGetter.js
+++ js/libs/provoda/utils/stateGetter.js
@@ -2,17 +2,34 @@ define(function (require) {
 'use strict';
 var spv = require('spv');
 var isSpecialState = require('./isSpecialState');
+var asNil = function (val) {
+  if (val == null) {
+    return
+  }
+
+  return val
+}
 
 var getTargetField = spv.getTargetField;
+var getFieldsTree = spv.getFieldsTree
 var stateGetter = spv.memorize(function stateGetter(state_path) {
   if (isSpecialState(state_path)) {
-    return function(states) {
-      return states[state_path];
+    return function getSpecialState(states) {
+      return asNil(states[state_path]);
     };
   }
-  return function(states) {
-    return getTargetField(states, state_path);
-  };
+  var path = getFieldsTree(state_path)
+  if (path.length != 1) {
+    return function getTreeState(states) {
+      // has asNil internally
+      return getTargetField(states, path);
+    }
+  }
+
+  var attr_name = path[0]
+  return function getSimpleState(states) {
+    return asNil(states[attr_name])
+  }
 });
 
 return stateGetter;
