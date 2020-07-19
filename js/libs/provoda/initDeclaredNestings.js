194f79b9a493b5df38358d750ae1293664e88d6f
Gleb Arestov <gr1b0k@gmail.com>
2020-07-19 18:37:00 +0300

    Remove dead code

--- js/libs/provoda/initDeclaredNestings.js
+++ js/libs/provoda/initDeclaredNestings.js
@@ -4,7 +4,6 @@ define(function(require) {
 var spv = require('spv');
 var parsePath = require('./routes/legacy/parse')
 var pathExecutor = require('./routes/legacy/stringify')
-var utils_simple = require('./utils/simple')
 
 var getTargetField = spv.getTargetField
 
