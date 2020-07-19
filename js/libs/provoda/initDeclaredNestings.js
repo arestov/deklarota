3890f78ca2ddc1c7e29d1b8201894b746c9a358f
Gleb Arestov <gr1b0k@gmail.com>
2020-07-19 19:22:14 +0300

    Move followStringTemplate

--- js/libs/provoda/initDeclaredNestings.js
+++ js/libs/provoda/initDeclaredNestings.js
@@ -4,6 +4,7 @@ define(function(require) {
 var spv = require('spv');
 var pathExecutor = require('./routes/legacy/stringify')
 var getParsedPath = require('./routes/legacy/getParsedPath')
+var followStringTemplate = require('./routes/legacy/followStringTemplate')
 
 var getTargetField = spv.getTargetField
 
@@ -50,31 +51,6 @@ var getPathBySimpleData = pathExecutor(function(chunkName, app, data) {
   return data && getTargetField(data, chunkName);
 });
 
-var followStringTemplate = function (app, md, obj, need_constr, full_path, strict, options, extra_states) {
-  if (obj.from_root) {
-    if (full_path === '') {
-      // used just "#" as path
-      return app;
-    }
-
-    // "#page/etc/etc"
-    return app.routePathByModels(full_path, app.start_page, need_constr, strict, options, extra_states);
-  }
-
-  if (obj.from_parent) {
-    // "^page/ect"
-    var target_md_start = md;
-    for (var i = 0; i < obj.from_parent; i++) {
-      target_md_start = target_md_start.map_parent;
-    }
-    if (!full_path) {
-      return target_md_start;
-    }
-    return app.routePathByModels(full_path, target_md_start, need_constr, strict, options, extra_states);
-  }
-
-  return app.routePathByModels(full_path, md, need_constr, strict, options, extra_states);
-};
 
 var executeStringTemplate = function(app, md, obj, need_constr, md_for_urldata) {
   var full_path = getPath(obj, app, md_for_urldata || md);
