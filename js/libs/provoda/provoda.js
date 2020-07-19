a04d76fc254cc52d0142f2f460709c548dba1e72
Gleb Arestov <gr1b0k@gmail.com>
2020-07-19 17:12:12 +0300

    Move HModel

--- js/libs/provoda/provoda.js
+++ js/libs/provoda/provoda.js
@@ -12,6 +12,7 @@ var Eventor = require('./Eventor');
 var StatesEmitter = require('./StatesEmitter');
 var CallbacksFlow = require('./CallbacksFlow');
 var Model = require('./Model');
+var HModel = require('./Model/HModel')
 var updateProxy = require('./updateProxy');
 var initDeclaredNestings = require('./initDeclaredNestings');
 var markStrucure = require('./structure/mark');
@@ -22,7 +23,6 @@ var mergeBhv = require('./provoda/_lmerge');
 var mpxUpdateAttr = require('./provoda/v/mpxUpdateAttr')
 
 var provoda, pv;
-var _updateAttr = require('_updateAttr');
 var DeathMarker = function() {
   //helper to find memory leaks; if there is memory leaking DeathMarker will be available in memory heap snapshot;
 };
@@ -74,7 +74,7 @@ pv = provoda = {
   Eventor: Eventor.PublicEventor,
   StatesEmitter: StatesEmitter,
   Model: Model,
-  HModel: getHModel(),
+  HModel: HModel,
   views_proxies: views_proxies,
   getOCF: function(propcheck, callback) {
     //init once
@@ -107,90 +107,6 @@ pv = provoda = {
 };
 
 
-function getHModel() {
-var HModel = spv.inh(Model, {
-  strict: true,
-  naming: function(fn) {
-    return function HModel(opts, data, params, more, states) {
-      fn(this, opts, data, params, more, states);
-    };
-  },
-  preinit: function(self, opts) {
-
-    //opts = opts || {};
-    if (!self.app){
-      self.app = null;
-    }
-
-    if (!self.map_parent){
-      self.map_parent = null;
-    }
-
-    if (!self.skip_map_init && !self.zero_map_level){
-      if (!opts || !opts.map_parent) {
-        if (!self.zero_map_level){
-          throw new Error('who is your map parent model?');
-        }
-      }
-    }
-
-    var map_parent = self.map_parent || opts.map_parent;
-
-    self.map_level_num = null;
-
-    if (self.zero_map_level) {
-      self.map_level_num = -1;
-    } else {
-      if (map_parent) {
-        self.map_level_num = map_parent.map_level_num + 1;
-      }
-    }
-
-    // self._super.apply(this, arguments);
-  },
-}, {
-  handling_v2_init: true,
-  network_data_as_states: true,
-  _hndOnPMDSwitch: function(e) {
-    this.checkPMDSwiched(e.value);
-  },
-  'stch-vswitched': function(target, state, old_state) {
-    if (state) {
-      var md = pv.getModelById(target, state);
-      _updateAttr(md, 'pmd_vswitched', true);
-    }
-    if (old_state) {
-      var old_md = pv.getModelById(target, old_state);
-      _updateAttr(old_md, 'pmd_vswitched', false);
-    }
-  },
-  switchPmd: function(toggle) {
-    var new_state;
-    if (typeof toggle == 'boolean')	{
-      new_state = toggle;
-    } else {
-      new_state = !this.state('pmd_vswitched');
-    }
-    var pmd_switch = this.getNesting('pmd_switch');
-    if (!pmd_switch) {return;}
-
-    if (new_state){
-      if (!this.state('pmd_vswitched')){
-        _updateAttr(pmd_switch, 'vswitched', this._provoda_id);
-      }
-    } else {
-      if (this.state('pmd_vswitched')){
-        _updateAttr(pmd_switch, 'vswitched', false);
-      }
-    }
-  },
-  checkPMDSwiched: function(value) {
-    _updateAttr(this, 'pmd_vswitched', value == this._provoda_id);
-  }
-});
-
-return HModel;
-}
 
 if ( typeof window === "object" && typeof window.document === "object" ) {
   window.provoda = provoda;
