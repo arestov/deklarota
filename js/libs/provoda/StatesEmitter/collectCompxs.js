cda02ab3285f1353005ee657def24ba19f7cb3be
Gleb Arestov <gr1b0k@gmail.com>
2020-07-25 18:29:07 +0300

    Change code to CompxAttrDecl

--- js/libs/provoda/StatesEmitter/collectCompxs.js
+++ js/libs/provoda/StatesEmitter/collectCompxs.js
@@ -42,7 +42,7 @@ var toParsedDeps = function(array) {
   return {fixed_deps: result, require_marks: require_marks}
 }
 
-var declr = function(comlx_name, cur) {
+var CompxAttrDecl = function(comlx_name, cur) {
   var item = cur instanceof Array ? fromArray(comlx_name, cur) : cur;
   var raw_depends_on = item.depends_on
 
@@ -51,31 +51,30 @@ var declr = function(comlx_name, cur) {
   }
 
   var parsed = toParsedDeps(raw_depends_on)
-  item.depends_on = parsed.fixed_deps
-  item.require_marks = parsed.require_marks
+  this.depends_on = parsed.fixed_deps
+  this.require_marks = parsed.require_marks
 
-  item.name = comlx_name;
+  this.name = comlx_name;
 
-  if (!item.depends_on.length && typeof item.fn !== 'function') {
+  if (!this.depends_on.length && typeof item.fn !== 'function') {
     throw new Error('use attr "input" to define default values')
   }
 
-  if (!item.fn) {
-    item.fn = identical;
-  }
-  if (!Array.isArray(item.depends_on)) {
-    throw new Error('should be list');
+  this.fn = item.fn || identical
+
+  if (!Array.isArray(this.depends_on)) {
+    throw new Error('should be list: ' + this.depends_on);
   }
 
-  item.watch_list = new Array(item.depends_on.length || 0);
+  this.watch_list = new Array(this.depends_on.length || 0);
 
-  for (var i = 0; i < item.depends_on.length; i++) {
-    if (!item.depends_on[i]) {
+  for (var i = 0; i < this.depends_on.length; i++) {
+    if (!this.depends_on[i]) {
       throw new Error('state name should not be empty');
     }
-    item.watch_list[i] = getShortStateName(item.depends_on[i]);
+    this.watch_list[i] = getShortStateName(this.depends_on[i]);
   }
-  return item;
+  return this;
 };
 
 
@@ -118,7 +117,7 @@ var extendTyped = function(self, typed_state_dcls) {
     if (!typed_state_dcls.hasOwnProperty(name)) {
       continue;
     }
-    extending_part[name] = declr(name, typed_state_dcls[name]);
+    extending_part[name] = new CompxAttrDecl(name, typed_state_dcls[name]);
   }
 
   result = cloneObj(result, extending_part);
