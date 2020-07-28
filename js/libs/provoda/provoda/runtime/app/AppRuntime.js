0fb762deeecd6ae99b80d91c1ebbb46873dd87c5
Gleb Arestov <gr1b0k@gmail.com>
2020-07-28 19:31:31 +0300

    Allow to report huge queue

--- js/libs/provoda/provoda/runtime/app/AppRuntime.js
+++ js/libs/provoda/provoda/runtime/app/AppRuntime.js
@@ -9,7 +9,11 @@ function AppRuntime(optionsRaw) {
   var options = optionsRaw || {}
 
   var glo = typeof global !== 'undefined' ? global : window
-  var flow = new CallbacksFlow({glo: glo, reportLongTask: options.reportLongTask})
+  var flow = new CallbacksFlow({
+    glo: glo,
+    reportLongTask: options.reportLongTask,
+    reportHugeQueue: options.reportHugeQueue
+  })
 
   this.models_counters = 1
   this.models = {}
