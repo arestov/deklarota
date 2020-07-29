d2834293fa4971d3325ca00c6179d7e08147e72c
Gleb Arestov <gr1b0k@gmail.com>
2020-07-29 12:41:00 +0300

    Create __nest_calculations cache

--- js/libs/provoda/dcl/nest_compx/init.js
+++ js/libs/provoda/dcl/nest_compx/init.js
@@ -3,16 +3,23 @@ define(function(require) {
 
 var Runner = require('./runner')
 
+var empty = {}
+
 return function (self) {
+  self.__nest_calculations = empty // show js engine real type
+  self.__nest_calculations = null
+
   if (!self._nest_by_type_listed || !self._nest_by_type_listed.compx) {
     return;
   }
+
+  self.__nest_calculations = {}
 
   var compx_list = self._nest_by_type_listed.compx
 
   for (var i = 0; i < compx_list.length; i++) {
     var cur = compx_list[i]
-    new Runner(self, cur);
+    self.__nest_calculations[cur.dest_name] = new Runner(self, cur);
   }
 }
 })
