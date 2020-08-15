acfe2285806b655835f5aa3c92d193a853762010
Gleb Arestov <gr1b0k@gmail.com>
2020-08-15 19:40:47 +0300

    Fix code style

--- js/utils/initApp.js
+++ js/utils/initApp.js
@@ -1,9 +1,7 @@
 define(require => {
   const startApp = require('../libs/provoda/provoda/runtime/app/start')
 
-  const initApp = (App, env, interfaces, { logger, ...busOptions } = {}) => {
-    return startApp({ App, interfaces }, { logger, env, ...busOptions })
-  }
+  const initApp = (App, env, interfaces, { logger, ...busOptions } = {}) => startApp({ App, interfaces }, { logger, env, ...busOptions })
 
   return initApp
 })
