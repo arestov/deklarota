558203eb77169ac17c91b47ad6249ea888590096
Gleb Arestov <gr1b0k@gmail.com>
2020-09-10 23:39:10 +0300

    refactor: create cachedField

---
+++ js/libs/provoda/dcl/cachedField.js
@@ -0,0 +1,31 @@
+const isNil = function isNil(arg) {
+  return arg == null
+}
+
+const cachedField = function(field, deps, final_compile, fn) {
+  if (final_compile !== true) {
+    throw new Error('make hasOwnProperty check for deps with final_compile=false')
+  }
+
+  return function checkCompiledField(model) {
+    if (model.hasOwnProperty(field)) {
+      return model[field]
+    }
+
+    var args = []
+    for (var i = 0; i < deps.length; i++) {
+      args[i] = model[deps[i]]
+    }
+
+
+    if (args.every(isNil)) {
+      model[field] = null
+      return model[field]
+    }
+
+    model[field] = fn(...args, model)
+
+    return model[field]
+  }
+}
+export default cachedField
--- js/libs/provoda/dcl/nest_compx/mentionsCandidates.js
+++ js/libs/provoda/dcl/nest_compx/mentionsCandidates.js
@@ -1,46 +1,38 @@
 import isGlueRoot from '../glue_rels/runtime/isGlueRoot'
 import isGlueParent from '../glue_rels/runtime/isGlueParent'
+import cachedField from '../cachedField'
 
-const collect = function(model) {
-  if (model._nest_by_type_listed == null) {
-    return null
-  }
-
-  var compx_list = model._nest_by_type_listed.comp
-  if (compx_list == null) {
-    return null
-  }
-
-  var result = []
-
-  for (var i = 0; i < compx_list.length; i++) {
-    var cur = compx_list[i]
-
-    for (var jj = 0; jj < cur.parsed_deps.nest_watch.length; jj++) {
-      var addr = cur.parsed_deps.nest_watch[jj]
-
-      result.push({
-        addr: addr,
-        dest_name: cur.dest_name,
-        final_rel_addr: cur.final_rel_addr,
-        final_rel_key: cur.final_rel_key,
-      })
+const getAllPossibleRelMentionsCandidates = cachedField(
+  '__rel_mentions_all_possible',
+  ['_nest_by_type_listed'],
+  true,
+  function getAllPossibleRelMentionsCandidates(_nest_by_type_listed) {
+    var compx_list = _nest_by_type_listed.comp
+    if (compx_list == null) {
+      return null
     }
+
+    var result = []
+
+    for (var i = 0; i < compx_list.length; i++) {
+      var cur = compx_list[i]
+
+      for (var jj = 0; jj < cur.parsed_deps.nest_watch.length; jj++) {
+        var addr = cur.parsed_deps.nest_watch[jj]
+
+        result.push({
+          addr: addr,
+          dest_name: cur.dest_name,
+          final_rel_addr: cur.final_rel_addr,
+          final_rel_key: cur.final_rel_key,
+        })
+      }
+    }
+
+    return result
   }
+)
 
-  return result
-}
-
-
-function getAllPossibleRelMentionsCandidates(model) {
-  if (model.hasOwnProperty('__rel_mentions_all_possible')) {
-    return model.__rel_mentions_all_possible
-  }
-
-  var result = collect(model)
-  model.__rel_mentions_all_possible = result
-  return result
-}
 
 const collectGlue = function(model) {
   if (model._nest_by_type_listed == null) {
