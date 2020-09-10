b58ad283943df2f50d834059c85f89d2d1f9c5ea
Gleb Arestov <gr1b0k@gmail.com>
2020-09-10 18:47:28 +0300

    refactor: move result in createUpdatedAddr

--- js/libs/provoda/utils/multiPath/createUpdatedAddr.js
+++ js/libs/provoda/utils/multiPath/createUpdatedAddr.js
@@ -2,30 +2,28 @@ import { getStateInfo, getNestInfo, getBaseInfo, updateResultType } from './pars
 import { doCopy } from '../../../spv/cloneObj'
 
 var createUpdatedAddr = function createUpdatedAddr(addr, part, string) {
+  const result = doCopy({}, addr)
+
   switch (part) {
     case 'state': {
-      const result = doCopy({}, addr)
       result.as_string = null
       result.state = getStateInfo(string)
       updateResultType(result)
       return result
     }
     case 'zip_name': {
-      const result = doCopy({}, addr)
       result.as_string = null
       result.zip_name = string
       updateResultType(result)
       return result
     }
     case 'nesting': {
-      const result = doCopy({}, addr)
       result.as_string = null
       result.nesting = getNestInfo(string)
       updateResultType(result)
       return result
     }
     case 'from_base': {
-      const result = doCopy({}, addr)
       result.as_string = null
       result.from_base = getBaseInfo(string)
       updateResultType(result)
