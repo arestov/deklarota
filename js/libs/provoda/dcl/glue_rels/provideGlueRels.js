4da36cdf1ea9660360f6e0c2552b0b5f8380e46d
Gleb Arestov <gr1b0k@gmail.com>
2020-09-11 07:51:55 +0300

    refactor: use getAllGlueSources in provideGlueRels

--- js/libs/provoda/dcl/glue_rels/provideGlueRels.js
+++ js/libs/provoda/dcl/glue_rels/provideGlueRels.js
@@ -1,3 +1,4 @@
+import { getAllGlueSources } from '../nest_compx/mentionsCandidates'
 import prepareGlueSourceRuntime from './runtime/prepare'
 import cachedField from '../cachedField'
 const emptyArray = Object.freeze([])
@@ -6,21 +7,8 @@ const provideGlueRels = cachedField(
   '__rel_all_glue_sources',
   ['_nest_by_type_listed'],
   true,
-  function provideGlueRels(_nest_by_type_listed) {
-    var comp_rels = _nest_by_type_listed && _nest_by_type_listed.comp
-    if (!comp_rels || !comp_rels.length) {
-      return emptyArray
-    }
-
-    var all_glue_sources = []
-
-    for (var i = 0; i < comp_rels.length; i++) {
-      var cur = comp_rels[i]
-      if (!cur.glue_sources || !cur.glue_sources.length) {
-        continue
-      }
-      all_glue_sources.push.apply(all_glue_sources, cur.glue_sources)
-    }
+  function provideGlueRels(_nest_by_type_listed, model) {
+    var all_glue_sources = getAllGlueSources(model) || []
     if (!all_glue_sources.length) {
       return emptyArray
     }
