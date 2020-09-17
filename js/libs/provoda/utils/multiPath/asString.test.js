f12a28afebe31c0ea54b0831eba836469cf4938b
Andrey Ivlev <andreyivlev@meetsidekick.com>
2020-09-17 23:32:10 +0300

    chore: rewrite provoda/utils/multiPath/asString.test.js

--- js/libs/provoda/utils/multiPath/asString.test.js
+++ js/libs/provoda/utils/multiPath/asString.test.js
@@ -1,38 +1,79 @@
 import parse from './parse'
 import asString from './asString'
 
-function pipe(str) {
-  return asString(parse(str))
-}
+describe('asString', () => {
+  describe('called on any valid addr', () => {
+    describe('with `as_string` set', () => {
+      it('should return `as_string` property value', () => {
+        expect(asString({ as_string: '< enabled <<< #' })).toEqual('< enabled <<< #')
+      })
+    })
 
-test('check asString', function() {
+    describe('with `as_string` unset', () => {
+      it('should cache result into `as_string` property', () => {
+        const parsed = parse('trivialAttr')
+        const serialized = asString(parsed)
+        expect(parsed.as_string).toEqual(serialized)
+      })
+    })
+  })
 
-  // "< state_name < nesting < resource < #"
-
-  expect(pipe('< @one:state_name < nesting < resource < #')).toMatchSnapshot()
-  expect(pipe('<< @one:nesting')).toMatchSnapshot()
-
-  // t.snapshot(parse("< state_name < @one:nesting < resource < #"));
-
-  expect(pipe('< state_name < nesting < resource < #')).toMatchSnapshot()
-
-  expect(pipe('< state_name << /resource/[:ddaf]/sdf < #')).toMatchSnapshot()
-
-  expect(pipe('< state_name <<< #')).toMatchSnapshot()
-
-  expect(pipe('<< nesting_name << #')).toMatchSnapshot()
-
-  expect(pipe('<< nesting_name << ^^')).toMatchSnapshot()
-
-  expect(pipe('< state_name <<< ^^')).toMatchSnapshot()
-
-  expect(pipe('< state_name')).toMatchSnapshot()
-
-  expect(pipe('state_name')).toMatchSnapshot()
-
-  expect(pipe('/resource/[:ddaf]/sdf < #')).toMatchSnapshot()
-
-  expect(pipe('/resource/[:ddaf]/sdf <')).toMatchSnapshot()
-
-  expect(pipe('nesting_name < < ^^')).toMatchSnapshot()
+  describe('smoke', () => {
+    it.each([
+      ['root route attr', [
+        '< @one:state_name < nesting < resource < #',
+        '< @one:state_name < nesting < resource < #',
+      ]],
+      ['nesting shortcut', [
+        '<< @one:nesting',
+        '<< @one:nesting <<',
+      ]],
+      ['full root query', [
+        '< state_name < nesting < resource < #',
+        '< state_name < nesting < resource < #',
+      ]],
+      ['full root query with route', [
+        '< state_name << /resource/[:ddaf]/sdf < #',
+        '< state_name << /resource/[:ddaf]/sdf < #',
+      ]],
+      ['root attr query', [
+        '< state_name <<< #',
+        '< state_name <<< #',
+      ]],
+      ['root nesting query', [
+        '<< nesting_name << #',
+        '<< nesting_name << #',
+      ]],
+      ['grandparent nesting query', [
+        '<< nesting_name << ^^',
+        '<< nesting_name << ^^',
+      ]],
+      ['grandparent attr query', [
+        '< state_name <<< ^^',
+        '< state_name <<< ^^',
+      ]],
+      ['attr query shortcut', [
+        '< state_name',
+        '< state_name <<<',
+      ]],
+      ['trivial attr query', [
+        'state_name',
+        '< state_name <<<',
+      ]],
+      ['root route query shortcut', [
+        '/resource/[:ddaf]/sdf < #',
+        '<<< /resource/[:ddaf]/sdf < #'
+      ]],
+      ['route shortcut', [
+        '/resource/[:ddaf]/sdf <',
+        '<<< /resource/[:ddaf]/sdf <',
+      ]],
+      ['grandparent nesting shortcut', [
+        'nesting_name << ^^',
+        '<< nesting_name << ^^'
+      ]]
+    ])('should work for %s', (title, [input, expected]) => {
+      expect(asString(parse(input))).toEqual(expected)
+    })
+  })
 })
