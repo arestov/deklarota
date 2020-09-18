c77ea53ca21ced6fdf94258ced76f50215c95b1e
Andrey Ivlev <andreyivlev@meetsidekick.com>
2020-09-18 12:52:19 +0300

    chore: rewrite provoda/utils/parse.test.js

--- js/libs/provoda/utils/multiPath/parse.test.js
+++ js/libs/provoda/utils/multiPath/parse.test.js
@@ -1,50 +1,32 @@
 import parse from './parse'
 
-test('check parsing', function() {
-  //
-  // "< state_name < nesting < resource < #"
+describe('parse', () => {
+  describe('smoke', () => {
+    it.each([
+      ['< @one:state_name < nesting < resource < #'],
+      ['< state_name < nesting < resource < #'],
+      ['< state_name << /resource/[:ddaf]/sdf < #'],
+      ['< state_name <<< #'],
+      ['<< nesting_name << #'],
+      ['<< nesting_name << ^^'],
+      ['< state_name <<< ^^'],
+      ['< state_name'],
+      ['state_name'],
+      ['@one:state_name:nest'],
+      ['@state_name:nest.test'],
+      ['/resource/[:ddaf]/sdf < #'],
+      ['/resource/[:ddaf]/sdf <'],
+      ['nesting_name < < ^^'],
+    ])('result should match snapshot for "%s"', input => {
+      expect(parse(input)).toMatchSnapshot()
+    })
 
-  expect(parse('< @one:state_name < nesting < resource < #')).toMatchSnapshot()
-
-  // t.snapshot(parse("< state_name < @one:nesting < resource < #"));
-
-  expect(function() {
-    parse('< state_name < @one:nesting < resource < #')
-  }).toThrow()
-
-  expect(function() {
-    parse('< @one: < nesting < resource < #')
-  }).toThrow()
-
-  expect(parse('< state_name < nesting < resource < #')).toMatchSnapshot()
-
-  expect(function() {
-    parse('< state_name < aggr:nesting < resource < #')
-  }).toThrow()
-
-  expect(parse('< state_name << /resource/[:ddaf]/sdf < #')).toMatchSnapshot()
-
-  expect(parse('< state_name <<< #')).toMatchSnapshot()
-
-  expect(parse('<< nesting_name << #')).toMatchSnapshot()
-
-  expect(parse('<< nesting_name << ^^')).toMatchSnapshot()
-
-  expect(parse('< state_name <<< ^^')).toMatchSnapshot()
-
-  expect(parse('< state_name')).toMatchSnapshot()
-
-  expect(parse('state_name')).toMatchSnapshot()
-
-  expect(parse('@one:state_name:nest')).toMatchSnapshot()
-  expect(parse('@state_name:nest.test')).toMatchSnapshot()
-
-
-  expect(parse('/resource/[:ddaf]/sdf < #')).toMatchSnapshot()
-
-  expect(parse('/resource/[:ddaf]/sdf <')).toMatchSnapshot()
-
-  expect(parse('nesting_name < < ^^')).toMatchSnapshot()
-
-
+    it.each([
+      ['< state_name < @one:nesting < resource < #'],
+      ['< @one: < nesting < resource < #'],
+      ['< state_name < aggr:nesting < resource < #'],
+    ])('should throw for "%s"', input => {
+      expect(() => parse(input)).toThrow()
+    })
+  })
 })
