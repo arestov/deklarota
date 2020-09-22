7f19dfbd377d0ae10e28f24b4eb7f83515ea9ed8
Vladislav Orlov <me@orlov-vo.ru>
2020-09-22 20:00:11 +0300

    feat: split distributive to separate subpackages

---
+++ core/src/main.js
@@ -0,0 +1,16 @@
+export { default as getAttr } from '../../js/libs/provoda/provoda/getAttr'
+export { default as updateAttr } from '../../js/libs/provoda/provoda/updateAttr'
+export { default as inputAttrs } from '../../js/libs/provoda/provoda/dcl/attrs/input'
+export { default as getNesting } from '../../js/libs/provoda/provoda/getNesting'
+export { default as getRel } from '../../js/libs/provoda/provoda/getRel'
+export { default as updateRel } from '../../js/libs/provoda/provoda/updateRel'
+export { default as bhv } from '../../js/libs/provoda/provoda/bhv'
+export { default as mergeBhv } from '../../js/libs/provoda/provoda/dcl/merge'
+export { default as appRoot } from '../../js/libs/provoda/provoda/appRoot'
+export { default as LoadableList } from '../../js/libs/provoda/provoda/LoadableList'
+
+// runtime
+export { default as prepareAppRuntime } from '../../js/libs/provoda/provoda/runtime/app/prepare'
+export { default as getModelById } from '../../js/libs/provoda/provoda/getModelById'
+export { default as FuncsQueue } from '../../js/libs/FuncsQueue'
+export { default as SyncReceiver } from '../../js/libs/provoda/provoda/SyncReceiver'
