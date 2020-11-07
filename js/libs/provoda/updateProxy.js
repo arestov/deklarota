1b7307ee36fd31539ec83508e4e4bb5c05bf7168
Gleb Arestov <gr1b0k@gmail.com>
2020-11-07 09:39:31 +0300

    refactor: move shallowEqual

--- js/libs/provoda/updateProxy.js
+++ js/libs/provoda/updateProxy.js
@@ -7,6 +7,7 @@ import _passHandleState from './dcl/passes/handleState/handle'
 import attrToRel from './dcl/nests/attrToRel'
 import deliverAttrQueryUpdates from './Model/mentions/deliverAttrQueryUpdates'
 import sameName from './sameName'
+import shallowEqual from './shallowEqual'
 
 var CH_GR_LE = 2
 
@@ -298,68 +299,6 @@ function getChanges(etr, total_original_states, start_from, changes_list, result
 
 
   // return changed_states;
-}
-
-function isSimpleObject(obj) {
-  if (obj == null) {
-    return false
-  }
-
-  // Allow arrays!
-  if (Array.isArray(obj)) {
-    return true
-  }
-
-  if (typeof obj != 'object') {
-    return false
-  }
-
-  if (obj.constructor != Object) {
-    // Don't allow custom instances like Date, URL, etc...
-    return false
-  }
-
-  return true
-}
-
-function shallowEqual(objA, objB) {
-  if (!isSimpleObject(objA) || !isSimpleObject(objB)) {
-    return false
-  }
-
-  const a_is_arr = Array.isArray(objA)
-  const b_is_arr = Array.isArray(objB)
-  if (a_is_arr != b_is_arr) {
-    return false
-  }
-
-  if (a_is_arr && objA.length != objB.length) {
-    return false
-  }
-
-  /*
-    fast check. just 1st level of props
-  */
-
-  for (var name in objA) {
-    if (objA.hasOwnProperty(name)) {
-      if (objA[name] !== objB[name]) {
-        return false
-      }
-    }
-  }
-
-  // objA and objB can have dirrenent keys number. so check both
-
-  for (var name in objB) {
-    if (objB.hasOwnProperty(name)) {
-      if (objB[name] !== objA[name]) {
-        return false
-      }
-    }
-  }
-
-  return true
 }
 
 function isSameValue(old_value, value) {
