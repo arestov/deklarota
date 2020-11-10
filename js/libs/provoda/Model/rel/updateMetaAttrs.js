02c29c545000881a29de370a48d07c88bdd65de8
Gleb Arestov <gr1b0k@gmail.com>
2020-11-10 14:35:52 +0300

    pref: don't use legacy meta attrs

--- js/libs/provoda/Model/rel/updateMetaAttrs.js
+++ js/libs/provoda/Model/rel/updateMetaAttrs.js
@@ -6,21 +6,15 @@ const updateMetaAttrs = function updateMetaAttrs(self, collection_name, array) {
     ? array.length
     : (array ? 1 : 0)
 
-  var name_for_length_legacy = collection_name + '$length'
   var name_for_length_modern = '$meta$nests$' + collection_name + '$length'
 
-  var name_for_exists_legacy = collection_name + '$exists'
   var name_for_exists_modern = '$meta$nests$' + collection_name + '$exists'
 
-  self._attrs_collector.defineAttr(name_for_length_legacy, 'int')
   self._attrs_collector.defineAttr(name_for_length_modern, 'int')
-  self._attrs_collector.defineAttr(name_for_exists_legacy, 'bool')
   self._attrs_collector.defineAttr(name_for_exists_modern, 'bool')
 
   _updateAttrsByChanges(self, [
-    name_for_length_legacy, count,
     name_for_length_modern, count,
-    name_for_exists_legacy, Boolean(count),
     name_for_exists_modern, Boolean(count),
   ])
 }
