3f875792639bc00f7e438041d54a22952311ff77
Gleb Arestov <gr1b0k@gmail.com>
2021-02-10 17:59:40 +0300

    refactor: don't use empty interface

--- js/models/Router.js
+++ js/models/Router.js
@@ -220,7 +220,9 @@ export default spv.inh(BasicRouter, {
 
 
 function initMapTree(target, start_page, needs_url_history, navi) {
-  target.useInterface('navi', needs_url_history && navi)
+  if (navi) {
+    target.useInterface('navi', needs_url_history && navi)
+  }
   _updateRel(target, 'navigation', [])
   _updateRel(target, 'start_page', start_page)
 
