05bfdfafe97b4cab3dee2a2a127d9ece2b63a99c
Gleb Arestov <gr1b0k@gmail.com>
2021-02-20 13:14:41 +0300

    fix: subscribe effects

--- js/libs/provoda/dcl/effects/legacy/subscribe/run/dispose.js
+++ js/libs/provoda/dcl/effects/legacy/subscribe/run/dispose.js
@@ -1,14 +1,15 @@
 function disposeSubscribeEffects(self) {
-  if (self._build_cache_interfaces == null) {
+  const _build_cache_interfaces = self.rt_schema._build_cache_interfaces
+  if (_build_cache_interfaces == null) {
     return
   }
 
-  for (var eff_key in self._build_cache_interfaces) {
-    if (!self._build_cache_interfaces.hasOwnProperty(eff_key)) {
+  for (var eff_key in _build_cache_interfaces) {
+    if (!_build_cache_interfaces.hasOwnProperty(eff_key)) {
       continue
     }
 
-    var dcl = self._build_cache_interfaces[eff_key]
+    var dcl = _build_cache_interfaces[eff_key]
     var key = self.getInstanceKey() + '-' + dcl.id
 
     delete self._highway._subscribe_effect_handlers[key]
--- js/libs/provoda/dcl/effects/legacy/subscribe/runOnApiRemoved.js
+++ js/libs/provoda/dcl/effects/legacy/subscribe/runOnApiRemoved.js
@@ -5,7 +5,7 @@ import makeBindChanges from './run/makeBindChanges'
 
 export default function(self, using_raw, interface_name, values_original) {
   var using = using_raw
-  using = self._interfaces_binders = markApi(self._interfaces_to_states_index, using, interface_name, false)
-  using = self._interfaces_binders = makeBindChanges(self, self._build_cache_interfaces, using, values_original)
+  using = self._interfaces_binders = markApi(self.rt_schema._interfaces_to_states_index, using, interface_name, false)
+  using = self._interfaces_binders = makeBindChanges(self, self.rt_schema._build_cache_interfaces, using, values_original)
   return using
 }
