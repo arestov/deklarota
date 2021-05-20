dc21ab64210b80886fff9ac966041430f34ddb46
Gleb Arestov <gr1b0k@gmail.com>
2021-05-20 21:22:46 +0300

    refactor: change deps_name calculation

--- js/libs/provoda/dcl/effects/legacy/api/dcl.js
+++ js/libs/provoda/dcl/effects/legacy/api/dcl.js
@@ -27,10 +27,10 @@ export default function ApiDeclr(name, data) {
     switch (data.length) {
       case 2: {
         var attr_deps = wrapDeps(data[0])
-        var attr_deps_name = '_triggered_api_' + name
 
         var all_deps = attr_deps
-        this.deps_name = attr_deps_name
+        var all_deps_name = '_api_all_needs_' + name
+        this.deps_name = all_deps_name
 
         this.fn = data[1]
         this.compxes = [
@@ -49,7 +49,8 @@ export default function ApiDeclr(name, data) {
         var needed_apis_dep_name = '_apis_need_for_' + name
 
         var all_deps = wrapDeps([attr_deps_name, needed_apis_dep_name])
-        this.deps_name = '_api_all_needs_' + name
+        var all_deps_name = '_api_all_needs_' + name
+        this.deps_name = all_deps_name
 
         this.compxes = [
           attr_deps_name, attr_deps,
