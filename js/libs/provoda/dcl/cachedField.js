d2d9f3ba67fba7167ed6beeb37d895f61a1d3a95
Gleb Arestov <gr1b0k@gmail.com>
2021-05-23 10:45:02 +0300

    refactor: improve check in getFinalResult

--- js/libs/provoda/dcl/cachedField.js
+++ js/libs/provoda/dcl/cachedField.js
@@ -14,6 +14,10 @@ const getFinalResult = (result) => {
     return sameArrayIfEmpty(result)
   }
 
+  if (result.constructor !== Object) {
+    return result
+  }
+
   if (Object.getOwnPropertySymbols(result).length) {
     return result
   }
