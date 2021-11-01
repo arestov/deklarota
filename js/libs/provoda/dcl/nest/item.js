c37c36de104cdc38188178cfb36eb9207329e2d6
Gleb Arestov <gr1b0k@gmail.com>
2021-11-01 10:54:04 +0300

    feat: make rel_shape from rel-nest data

--- js/libs/provoda/dcl/nest/item.js
+++ js/libs/provoda/dcl/nest/item.js
@@ -3,6 +3,33 @@ import relShape from '../nests/relShape'
 import constr_mention from '../../structure/constr_mention'
 var declarationConstructor = constr_mention.declarationConstructor
 
+
+const subPageToLink = (item) => {
+  switch (item.type) {
+    case 'route': {
+      return {
+        type: 'addr',
+        value: item.value,
+      }
+    }
+    case 'constr': {
+      return {
+        type: 'constr',
+        value: item.key,
+      }
+    }
+    default: throw new Error('unknown item type')
+  }
+}
+
+const subPagesToRelShapeLinks = (list) => {
+  if (!Array.isArray(list)) {
+    return subPageToLink(list)
+  }
+
+  return list.map(subPageToLink)
+}
+
 var NestDcl = function(name, data) {
   this.nesting_name = name
   this.subpages_names_list = declarationConstructor(name, data[0], 'nest-')
@@ -10,7 +37,11 @@ var NestDcl = function(name, data) {
   this.ask_for = null
   this.idle_until = null
   this.preload_on = null
-  this.rel_shape = null
+
+
+  this.rel_shape = relShape({
+    linking: subPagesToRelShapeLinks(this.subpages_names_list)
+  })
 
   if (!data[1] && !data[2]) {
     return
@@ -20,7 +51,6 @@ var NestDcl = function(name, data) {
     this.ask_for = data[1].ask_for || null
     this.idle_until = data[1].idle_until || this.ask_for || null
     this.preload_on = data[1].preload_on || null
-    this.rel_shape = relShape(data[2])
   } else {
     console.warn('fix legacy `nest-` dcl', data[1], data[2])
     var preload = data[1]
