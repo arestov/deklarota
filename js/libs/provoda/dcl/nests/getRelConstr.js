42c97e85b7ea1b1a62bc5ff4d471b40b28284fcb
Gleb Arestov <gr1b0k@gmail.com>
2021-11-02 18:45:27 +0300

    feat: handle list of constr_linking

--- js/libs/provoda/dcl/nests/getRelConstr.js
+++ js/libs/provoda/dcl/nests/getRelConstr.js
@@ -17,35 +17,52 @@ const getAddrRelConstr = (prt, rel_path) => {
   return cur
 }
 
-const recurGetConstrByRelLinking = (getAddrRelConstr) => (prt, constr_linking) => {
-  if (constr_linking == null) {
-    return null
+const recurGetConstrByRelLinking = (getAddrRelConstr) => {
+
+  const oneItem = (prt, linking_item) => {
+    if (linking_item.type == 'constr') {
+      return prt._all_chi[linking_item.value]
+    }
+
+    if (linking_item.type != 'addr') {
+      throw new Error('unknown type')
+    }
+
+    const addr = linking_item.value
+    if (addr.base_itself) {
+      return prt.constructor
+    }
+
+    var base = getBasePrtByAddr(prt, addr)
+
+    if (!addr.nesting.path) {
+      return base.constructor
+    }
+
+    return getAddrRelConstr(base, addr.nesting.path)
   }
 
-  if (Array.isArray(constr_linking)) {
-    throw new Error('implement list of constr_linking')
+  return (prt, constr_linking) => {
+    if (constr_linking == null) {
+      return null
+    }
+
+    if (!Array.isArray(constr_linking)) {
+      return oneItem(prt, constr_linking)
+    }
+
+    let result = []
+    for (var i = 0; i < constr_linking.length; i++) {
+      const one = oneItem(prt, constr_linking[i])
+      if (!Array.isArray(one)) {
+        result.push(one)
+      } else {
+        result.push(...one)
+      }
+    }
+
+    return result.length ? result : null
   }
-
-  if (constr_linking.type == 'constr') {
-    return prt._all_chi[constr_linking.value]
-  }
-
-  if (constr_linking.type != 'addr') {
-    throw new Error('unknown type')
-  }
-
-  const addr = constr_linking.value
-  if (addr.base_itself) {
-    return prt.constructor
-  }
-
-  var base = getBasePrtByAddr(prt, addr)
-
-  if (!addr.nesting.path) {
-    return base.constructor
-  }
-
-  return getAddrRelConstr(base, addr.nesting.path)
 }
 
 const getRelConstrByRelLinking = recurGetConstrByRelLinking(getAddrRelConstr)
