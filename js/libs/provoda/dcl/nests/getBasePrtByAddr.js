79797d787c4703c3d33d24320485e265b9c2f665
Gleb Arestov <gr1b0k@gmail.com>
2021-11-02 15:46:35 +0300

    refactor: move getBasePrtByAddr

---
+++ js/libs/provoda/dcl/nests/getBasePrtByAddr.js
@@ -0,0 +1,41 @@
+import routePathByModels from '../../routePathByModels'
+
+const getAddrBasePrt = (prt, from_base) => {
+  if (!from_base || !from_base.type) {
+    return prt
+  }
+
+  switch (from_base.type) {
+    case 'root': {
+      return prt.RootConstr.prototype.start_page
+    }
+    case 'parent': {
+      let count = from_base.steps
+      let result = prt
+      while (count) {
+        result = result._parent_constr.prototype
+        count = count - 1
+      }
+      return result
+    }
+    default: {
+      throw new Error('unknown type ' + from_base.type)
+    }
+  }
+}
+
+
+const getBasePrtByAddr = (prt, addr) => {
+  var base = getAddrBasePrt(prt, addr.from_base)
+
+  if (!addr.resource.path) {
+    return base
+  }
+
+  const constr = routePathByModels(base, addr.resource.path, true)
+
+
+  return constr && constr.prototype
+}
+
+export default getBasePrtByAddr
--- js/libs/provoda/dcl/nests/getRelConstr.js
+++ js/libs/provoda/dcl/nests/getRelConstr.js
@@ -1,30 +1,6 @@
-import routePathByModels from '../../routePathByModels'
 import { getNestingConstr } from '../../structure/get_constr'
 import getRelShape from './getRelShape'
-
-const getAddrBasePrt = (prt, from_base) => {
-  if (!from_base || !from_base.type) {
-    return prt
-  }
-
-  switch (from_base.type) {
-    case 'root': {
-      return prt.RootConstr.prototype.start_page
-    }
-    case 'parent': {
-      let count = from_base.steps
-      let result = prt
-      while (count) {
-        result = result._parent_constr.prototype
-        count = count - 1
-      }
-      return result
-    }
-    default: {
-      throw new Error('unknown type ' + from_base.type)
-    }
-  }
-}
+import getBasePrtByAddr from './getBasePrtByAddr'
 
 const getAddrRelConstr = (prt, rel) => {
   let cur = prt
@@ -41,18 +17,6 @@ const getAddrRelConstr = (prt, rel) => {
   return cur
 }
 
-const getBasePrtByAddr = (prt, addr) => {
-  var base = getAddrBasePrt(prt, addr.from_base)
-
-  if (!addr.resource.path) {
-    return base
-  }
-
-  const constr = routePathByModels(base, addr.resource.path, true)
-
-
-  return constr && constr.prototype
-}
 
 const getRelConstrByRelLinking = (prt, constr_linking) => {
   if (constr_linking == null) {
