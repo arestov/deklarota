77d2faa6fb7643e399a5de8cf522c23d043aa56d
Gleb Arestov <gr1b0k@gmail.com>
2021-11-03 11:16:38 +0300

    refactor: remove dead code

--- js/libs/provoda/dcl/nests/getRelConstr.js
+++ js/libs/provoda/dcl/nests/getRelConstr.js
@@ -1,22 +1,4 @@
-import { getNestingConstr } from '../../structure/get_constr'
-import getRelShape from './getRelShape'
 import getBasePrtByAddr from './getBasePrtByAddr'
-
-const getAddrRelConstr = (prt, rel_path) => {
-  let cur = prt
-  for (var i = 0; i < rel_path.length; i++) {
-    const rel_name = rel_path[i]
-
-    cur = getRelConstr(cur, rel_name)
-    cur = cur && cur.prototype
-    if (!cur) {
-      console.log('🤼‍♂️problem', rel_path, rel_name)
-      break
-    }
-  }
-
-  return cur && cur.constructor
-}
 
 const recurGetConstrByRelLinking = (getAddrRelConstr) => {
 
@@ -66,38 +48,5 @@ const recurGetConstrByRelLinking = (getAddrRelConstr) => {
   }
 }
 
-const getRelConstrByRelLinking = recurGetConstrByRelLinking(getAddrRelConstr)
 
-const getRelConstrByRef = (self, rel_name) => {
-  const rel_shape = getRelShape(self, rel_name)
-  if (!rel_shape || !rel_shape.constr_linking) {
-    return
-  }
-
-  const constr_linking = rel_shape.constr_linking
-  return getRelConstrByRelLinking(self, constr_linking)
-
-}
-
-function getRelConstr(prt, rel_name) {
-  if (!prt.RootConstr) {
-    debugger
-  }
-
-  var by_ref = getRelConstrByRef(prt, rel_name)
-  if (by_ref) {
-    return by_ref
-  }
-
-
-  const Constr = getNestingConstr(prt.RootConstr.prototype, prt, rel_name)
-  var result = Constr && Constr.prototype
-  if (!result) {
-    // find by ref rel addr
-  }
-
-  return result
-}
-
-export { recurGetConstrByRelLinking, getBasePrtByAddr, getRelConstrByRelLinking }
-export default getRelConstr
+export { recurGetConstrByRelLinking }
