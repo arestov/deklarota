01586cc0691c1373b8792e2fb4b6ac0645429dca
Gleb Arestov <gr1b0k@gmail.com>
2021-11-03 18:45:35 +0300

    feat: handle * validate arg_nest_ base for actions

--- js/libs/provoda/dcl/passes/validateActionsDestinations.js
+++ js/libs/provoda/dcl/passes/validateActionsDestinations.js
@@ -1,19 +1,53 @@
-import getPrtsByRelPath from '../nests/getPrtsByRelPath'
+import getRelShape from '../nests/getRelShape'
+import getPrtsByRelPath, { getRelConstrByRelLinking } from '../nests/getPrtsByRelPath'
 import getBasePrtByAddr from '../nests/getBasePrtByAddr'
 
-const validateActionTarget = (self, target) => {
-  if (target.target_path.result_type != 'nesting') {
-    return
+const throwError = (msg, item, context) => {
+  const err = new Error(msg)
+  console.error(err.message, context, item.__code_path)
+  throw err
+}
+
+const baseArgNestingStage = (prt, target_options, action) => {
+  if (!target_options?.base) {
+    return prt
   }
 
+  const rel_shape = action.rel_shape || getRelShape(prt, action.rel_name)
+
+  if (!rel_shape) {
+    throwError('can\'t get rel_shape for action', prt, {action})
+  }
+
+  return getRelConstrByRelLinking(prt, rel_shape.constr_linking)
+}
+
+const oneBase = (self, target) => {
   const addr = target.target_path
 
   const base = getBasePrtByAddr(self, addr)
-  const rel_constr = getPrtsByRelPath(
+  return getPrtsByRelPath(
     base,
     addr.nesting.path,
     false
   )
+}
+
+const validateActionTarget = (self, target, action) => {
+  if (target.target_path.result_type != 'nesting') {
+    return
+  }
+
+  const arg_nesting_base = baseArgNestingStage(self, target.options, action)
+
+  if (!Array.isArray(arg_nesting_base)) {
+    oneBase(arg_nesting_base, target)
+    return
+  }
+
+  for (var i = 0; i < arg_nesting_base.length; i++) {
+    oneBase(arg_nesting_base[i], target)
+  }
 
 
 
@@ -36,12 +70,12 @@ const validateActionsDestinations = (Root, self) => {
 
     if (cur.targeted_results_list) {
       for (var i = 0; i < cur.targeted_results_list.length; i++) {
-        validateActionTarget(self, cur.targeted_results_list[i])
+        validateActionTarget(self, cur.targeted_results_list[i], cur)
       }
     }
 
     if (cur.targeted_single_result) {
-      validateActionTarget(self, cur.targeted_single_result)
+      validateActionTarget(self, cur.targeted_single_result, cur)
     }
 
 
