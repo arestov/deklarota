26d3f2decb8173816489f34be54245507c250a93
Gleb Arestov <gr1b0k@gmail.com>
2021-11-23 17:24:33 +0300

    style: fix initBrowsing

--- js/utils/initBrowsing.js
+++ js/utils/initBrowsing.js
@@ -1,19 +1,6 @@
 import BrowseMap from '../libs/BrowseMap'
 import _updateRel from '../libs/provoda/_internal/_updateRel'
-import animateMapChanges from '../libs/provoda/dcl/probe/animateMapChanges'
-
-function initBrowsing(app, states) {
-  const map = BrowseMap.hookRoot(app, app.start_page, states)
-  app.map = map
-
-  initMapTree(app, app.start_page)
-
-  const bwlev = BrowseMap.showInterest(map, [])
-  BrowseMap.changeBridge(bwlev)
-
-  return map
-}
-
+// import animateMapChanges from '../libs/provoda/dcl/probe/animateMapChanges'
 
 function initMapTree(app, start_page) {
 // app.useInterface('navi', needs_url_history && navi);
@@ -22,6 +9,20 @@ function initMapTree(app, start_page) {
 
 
   return app.map
+}
+
+
+function initBrowsing(app, states) {
+  const map = BrowseMap.hookRoot(app, app.start_page, states)
+  // eslint-disable-next-line fp/no-mutation, no-param-reassign
+  app.map = map
+
+  initMapTree(app, app.start_page)
+
+  const bwlev = BrowseMap.showInterest(map, [])
+  BrowseMap.changeBridge(bwlev)
+
+  return map
 }
 
 export default initBrowsing
