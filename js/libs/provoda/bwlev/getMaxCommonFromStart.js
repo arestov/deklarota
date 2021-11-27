52840c8a58eedb0e8f180c29caec1ec19bcf39e4
Gleb Arestov <gr1b0k@gmail.com>
2021-11-27 11:36:20 +0300

    fix: don't access array items out of range

--- js/libs/provoda/bwlev/getMaxCommonFromStart.js
+++ js/libs/provoda/bwlev/getMaxCommonFromStart.js
@@ -1,5 +1,5 @@
 const getMaxCommonFromStart = (arrA, arrB) => {
-  const length = Math.max(arrA.length, arrB.length)
+  const length = Math.min(arrA.length, arrB.length)
   for (let i = 0; i < length; i++) {
     const curA = arrA[i]
     const curB = arrB[i]
--- js/libs/provoda/bwlev/getMaxCommonFromStart.test.js
+++ js/libs/provoda/bwlev/getMaxCommonFromStart.test.js
@@ -7,4 +7,7 @@ it('getClosestStep', () => {
   expect(getMaxCommonFromStart([1, 77, 89], [1, 2, 3])).toMatchSnapshot()
   expect(getMaxCommonFromStart([9, 77, 89], [1, 2, 3])).toMatchSnapshot()
   expect(getMaxCommonFromStart([1, 2, 3], [1, 2, 4])).toMatchSnapshot()
+
+  expect(getMaxCommonFromStart([1, 2, 3, 4, 5, 6], [1, 2, 3])).toMatchSnapshot()
+
 })
