b6eb9e7eb56ef334e9917731e02426bac58ab5ed
Gleb Arestov <gr1b0k@gmail.com>
2021-11-28 14:36:20 +0300

    fix: make sure that bwlev is properly inserted

--- js/views/map_slice/handleNavChange.js
+++ js/views/map_slice/handleNavChange.js
@@ -1,4 +1,19 @@
 import getModelFromR from '../../libs/provoda/provoda/v/getModelFromR'
+import getLevByBwlev from './getLevelContainer'
+import dom_helpers from '../../libs/provoda/utils/dom_helpers'
+
+const ensureCorrectBwlevContainer = (level_con, view) => {
+  /*
+    this code could be unnecessary
+    due reappending code in View/appending.js
+  */
+  const view_con = view.getCNode()
+  const lev_con_node = dom_helpers.unwrap(level_con.material)
+
+  if (view_con.parentNode !== lev_con_node) {
+    lev_con_node.appendChild(view_con)
+  }
+}
 
 const handleNavChange = (self, change) => {
   const mpx = self.getStoredMpx(getModelFromR(self, change.bwlev))
@@ -13,6 +28,12 @@ const handleNavChange = (self, change) => {
     }
     case 'travebasing-update': {
       self.setVMpshow(mpx, true)
+      const views = self.getChildViewsByMpx(mpx)
+      const level_con = getLevByBwlev(self, change.bwlev)
+      for (let i = 0; i < views.length; i++) {
+        ensureCorrectBwlevContainer(level_con, views[i])
+      }
+
       return
     }
     case 'travebasing-add': {
