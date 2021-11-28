e91fb06935a64a6869f06e76168d001e71e7d704
Gleb Arestov <gr1b0k@gmail.com>
2021-11-28 13:55:23 +0300

    refactor: move getLevelContainer

--- js/views/map_slice/MapSliceSpyglassCore.js
+++ js/views/map_slice/MapSliceSpyglassCore.js
@@ -1,7 +1,7 @@
 
 import View from '../../libs/provoda/View'
 import spv from '../../libs/spv'
-import css from './css'
+
 import updateAttr from '../../libs/provoda/provoda/updateAttr'
 import _updateAttr from '../../libs/provoda/_internal/_updateAttr'
 import mpxUpdateAttr from '../../libs/provoda/provoda/v/mpxUpdateAttr'
@@ -16,45 +16,15 @@ import readMapSliceAnimationData from './readMapSliceAnimationData'
 import animateMapSlice, { getLevNum } from './animateMapSlice'
 import findMpxViewInChildren from './findMpxViewInChildren'
 import handleNavChange from './handleNavChange'
-
-const can_animate = css.transform && css.transition
+import getLevelContainer from './getLevelContainer'
 
 const last = (list) => list && list[list.length - 1]
-
-const LevContainer = function(con, scroll_con, material, tpl, context) {
-  this.c = con
-  this.scroll_con = scroll_con
-  this.material = material
-  this.tpl = tpl
-  this.context = context
-  this.callbacks = []
-  const _this = this
-  if (can_animate) {
-    spv.addEvent(this.c[0], can_animate, function() {
-      //console.log(e);
-      _this.completeAnimation()
-    })
-  }
-}
 
 const viewOnLevelP = function(bwlev, view) {
   const lev_conj = this.getLevByBwlev(bwlev, view.nesting_space == 'detailed')
   view.wayp_scan_stop = true
   return lev_conj.material
 }
-
-LevContainer.prototype = {
-  onTransitionEnd: function(cb) {
-    this.callbacks.push(cb)
-  },
-  completeAnimation: function() {
-    while (this.callbacks.length) {
-      const cb = this.callbacks.shift()
-      this.context.nextLocalTick(cb)
-    }
-  }
-}
-
 
 export default spv.inh(View, {
   init: function(self) {
@@ -171,70 +141,8 @@ export default spv.inh(View, {
     }
   },
   getLevByBwlev: function(bwlev, deeper) {
-    return this.getLevelContainer(bwlev, deeper)
+    return getLevelContainer(this, bwlev, deeper)
 
-  },
-  getLevelContainer: function(bwlev, deeper) {
-    const raw_num = bwlev.getAttr('map_level_num')
-    if (raw_num < -1) {
-      throw new Error('wrong map_level_num')
-    }
-
-    const is_very_start = bwlev.getAttr('is_main_perspectivator_resident')
-    const num_erl = raw_num + (deeper ? 1 : 0)
-    if (num_erl == -1 && is_very_start) {
-      return this.lev_containers.start_page
-    }
-
-    if (deeper) {
-      throw new Error('wont use `deeper` here')
-    }
-
-    const num = raw_num
-    if (this.lev_containers[num]) {
-      return this.lev_containers[num]
-    } else {
-      /*
-      if (!view){
-        throw new Error('give me "view"');
-      }*/
-      if (num == -1 && !this.lev_containers.start_page) {
-        throw new Error('start_screen must exist')
-      }
-
-      const node = this.root_view.getSample('complex-page')
-
-      const tpl = this.parent_view.pvtemplate(node, false, false, {
-        '$lev_num': num
-      })
-
-      this.addTpl(tpl)
-
-      let next_lev_con
-      for (let i = num; i <= this.max_level_num; i++) {
-        if (this.lev_containers[i]) {
-          next_lev_con = this.lev_containers[i]
-          break
-        }
-      }
-      if (next_lev_con) {
-        node.insertBefore(next_lev_con.c)
-      } else {
-        node.appendTo(this.getInterface('app_map_con'))
-      }
-
-      const lev_con = new LevContainer(
-        node,
-        tpl.ancs['scroll_con'],
-        tpl.ancs['material'],
-        tpl,
-        this
-      )
-      this.lev_containers[num] = lev_con
-
-      this.max_level_num = Math.max(this.max_level_num, num)
-      return lev_con
-    }
   },
   wrapStartScreen: function(start_screen_node) {
     const start_screen = $(start_screen_node)
