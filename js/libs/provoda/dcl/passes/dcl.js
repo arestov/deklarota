bcd7143f2a36e407815abf777efac051f391f760
Gleb Arestov <gr1b0k@gmail.com>
2022-01-14 10:34:28 +0300

    refactor: separate readingDeps to file

--- js/libs/provoda/dcl/passes/dcl.js
+++ js/libs/provoda/dcl/passes/dcl.js
@@ -1,10 +1,10 @@
 
-import parseMultiPath from '../../utils/multiPath/parse'
 import noop from './noop'
-import now from './deps/now'
 import targetedResult from './targetedResult/dcl'
 import RelShape from '../nests/relShape'
-import emptyArray from '../../emptyArray'
+import { readingDeps } from './readingDeps'
+
+
 // var utils = require('../../utils/index.js');
 // var getParsedState = utils.getParsedState
 
@@ -56,42 +56,6 @@ import emptyArray from '../../emptyArray'
 //         }
 //     }
 // },
-
-
-const readingDeps = function getDeps(optionalNames) {
-  return (deps) => {
-    if (!deps || !deps.length) {
-      return emptyArray
-    }
-
-    const result = new Array(deps.length)
-    for (let i = 0; i < deps.length; i++) {
-
-
-      if (deps[i] === '$noop') {
-        if (!optionalNames?.hasOwnProperty(deps[i])) {
-          throw new Error(`${deps[i]} can't be used here`)
-        }
-        result[i] = optionalNames[deps[i]]
-        continue
-      }
-
-      if (deps[i] === '$now') {
-        result[i] = now
-        continue
-      }
-
-      const cur = parseMultiPath(deps[i], true)
-      result[i] = cur
-
-      if (cur.nesting && cur.nesting.path && !cur.zip_name) {
-        throw new Error('zip name `@one:` or `@all:` should be provided for: ' + deps[i])
-      }
-
-    }
-    return result
-  }
-}
 
 const getDeps = readingDeps({'$noop': noop})
 
---
+++ js/libs/provoda/dcl/passes/readingDeps.js
@@ -0,0 +1,38 @@
+import parseMultiPath from '../../utils/multiPath/parse'
+import now from './deps/now'
+import emptyArray from '../../emptyArray'
+
+export const readingDeps = function getDeps(optionalNames) {
+  return (deps) => {
+    if (!deps || !deps.length) {
+      return emptyArray
+    }
+
+    const result = new Array(deps.length)
+    for (let i = 0; i < deps.length; i++) {
+
+
+      if (deps[i] === '$noop') {
+        if (!optionalNames?.hasOwnProperty(deps[i])) {
+          throw new Error(`${deps[i]} can't be used here`)
+        }
+        result[i] = optionalNames[deps[i]]
+        continue
+      }
+
+      if (deps[i] === '$now') {
+        result[i] = now
+        continue
+      }
+
+      const cur = parseMultiPath(deps[i], true)
+      result[i] = cur
+
+      if (cur.nesting && cur.nesting.path && !cur.zip_name) {
+        throw new Error('zip name `@one:` or `@all:` should be provided for: ' + deps[i])
+      }
+
+    }
+    return result
+  }
+}
