7870ee89abea577ae82028c0e1389b372c9a4937
Gleb Arestov <gr1b0k@gmail.com>
2022-01-14 10:31:26 +0300

    refactor: create readingDeps

--- js/libs/provoda/dcl/passes/dcl.js
+++ js/libs/provoda/dcl/passes/dcl.js
@@ -57,33 +57,43 @@ import emptyArray from '../../emptyArray'
 //     }
 // },
 
-const getDeps = function(deps) {
-  if (!deps || !deps.length) {
-    return emptyArray
+
+const readingDeps = function getDeps(optionalNames) {
+  return (deps) => {
+    if (!deps || !deps.length) {
+      return emptyArray
+    }
+
+    const result = new Array(deps.length)
+    for (let i = 0; i < deps.length; i++) {
+
+
+      if (deps[i] === '$noop') {
+        if (!optionalNames?.hasOwnProperty(deps[i])) {
+          throw new Error(`${deps[i]} can't be used here`)
+        }
+        result[i] = optionalNames[deps[i]]
+        continue
+      }
+
+      if (deps[i] === '$now') {
+        result[i] = now
+        continue
+      }
+
+      const cur = parseMultiPath(deps[i], true)
+      result[i] = cur
+
+      if (cur.nesting && cur.nesting.path && !cur.zip_name) {
+        throw new Error('zip name `@one:` or `@all:` should be provided for: ' + deps[i])
+      }
+
+    }
+    return result
   }
-
-  const result = new Array(deps.length)
-  for (let i = 0; i < deps.length; i++) {
-    if (deps[i] === '$noop') {
-      result[i] = noop
-      continue
-    }
-
-    if (deps[i] === '$now') {
-      result[i] = now
-      continue
-    }
-
-    const cur = parseMultiPath(deps[i], true)
-    result[i] = cur
-
-    if (cur.nesting && cur.nesting.path && !cur.zip_name) {
-      throw new Error('zip name `@one:` or `@all:` should be provided for: ' + deps[i])
-    }
-
-  }
-  return result
 }
+
+const getDeps = readingDeps({'$noop': noop})
 
 function same(arg) {
   return arg
