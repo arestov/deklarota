35a0b1b314d585b25bcd6b214ded3f41c63aaaa2
Gleb Arestov <gr1b0k@gmail.com>
2022-01-17 10:14:21 +0300

    refactor: remove dead code (handling_v2_init = false case)

--- js/libs/provoda/Model/initConstr/subitem.js
+++ js/libs/provoda/Model/initConstr/subitem.js
@@ -1,6 +1,6 @@
 
 
-export default function initSi(Constr, parent_md, data, params, more, states) {
+export default function initSi(Constr, parent_md, data) {
   if (Constr.prototype.conndst_parent && Constr.prototype.conndst_parent.length) {
     if (Constr.prototype.pconstr_id !== true && parent_md.constr_id !== Constr.prototype.pconstr_id) {
       console.log((new Error('pconstr_id should match constr_id')).stack)
@@ -13,7 +13,7 @@ export default function initSi(Constr, parent_md, data, params, more, states) {
     const initsbi_opts = parent_md.getSiOpts()
 
     parent_md.useMotivator(instance, function(instance) {
-      instance.init(initsbi_opts, data, params, more, states)
+      instance.init(initsbi_opts, data)
     })
 
     return instance
@@ -29,7 +29,7 @@ export default function initSi(Constr, parent_md, data, params, more, states) {
     app: parent_md.app
   }
 
-  const instancePure = new Constr(opts, data, params, more, states)
+  const instancePure = new Constr(opts, data)
 
   instancePure.current_motivator = null
 
