5a61fcc71a565510170f8dd2a8de61e2c16c9412
Gleb Arestov <gr1b0k@gmail.com>
2022-04-02 12:50:44 +0300

    refactor: create requireLazyRel

---
+++ js/libs/provoda/utils/requireLazyRel.js
@@ -0,0 +1,26 @@
+import getRelShape from '../dcl/nests/getRelShape'
+import updateNesting from '../Model/updateNesting'
+import getNesting from '../provoda/getNesting'
+import { getNestingConstr } from '../structure/get_constr'
+
+const requireLazyRel = (self, rel_name) => {
+  const value = getNesting(self, rel_name)
+  if (value != null) {
+    return value
+  }
+
+  const Constr = getNestingConstr(self.app, self, rel_name)
+  const item = self.initSi(Constr, {
+    by: 'requireLazyRel',
+    init_version: 2,
+    attrs: null,
+    head: null,
+    url_params: null,
+  })
+
+  const { many } = getRelShape(self, rel_name)
+  updateNesting(self, rel_name, many ? [item] : item)
+  return item
+}
+
+export default requireLazyRel
