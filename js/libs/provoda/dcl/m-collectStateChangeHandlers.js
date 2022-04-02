f7efb7ad95f85b3220075a6f323bcd1e3b1da998
Gleb Arestov <gr1b0k@gmail.com>
2022-04-02 19:06:31 +0300

    refactor: deprecate using comp stch

--- js/libs/provoda/dcl/m-collectStateChangeHandlers.js
+++ js/libs/provoda/dcl/m-collectStateChangeHandlers.js
@@ -1,27 +1,7 @@
 
 import spv from '../../spv'
-import NestWatch from '../nest-watch/NestWatch'
-import toMultiPath from '../utils/NestingSourceDr/toMultiPath'
 import collectStateChangeHandlers from './collectStateChangeHandlers'
-import standart from '../nest-watch/standartNWH'
 const splitByDot = spv.splitByDot
-
-const wrapper = standart(function wrapper(md, items, lnwatch) {
-  const callback = lnwatch.nwatch.handler.stch_fn
-  callback(md, null, null, {
-    items: items,
-    item: null
-  })
-})
-
-const stateHandler = standart(function baseStateHandler(md, items, lnwatch, args) {
-  if (!args.length) {return}
-  const callback = lnwatch.nwatch.handler.stch_fn
-  callback(md, args[1], args[2], {
-    items: items,
-    item: args[3]
-  })
-})
 
 
 const getParsedStateChange = spv.memorize(function getParsedStateChange(string) {
@@ -48,15 +28,9 @@ export default function(self, props) {
 
     const nw_draft2 = getParsedStateChange(stname)
     if (!nw_draft2) { continue }
+    const err = new Error('wrap `stch` in comp attr')
+    console.log({ attr: stname }, err)
 
-    const callback = index[stname]
-
-    self.st_nest_matches.push(
-      new NestWatch(toMultiPath({selector: nw_draft2.selector}), nw_draft2.state, {
-        onchd_state: stateHandler,
-        onchd_count: wrapper,
-        stch_fn: callback,
-      })
-    )
+    throw err
   }
 }
