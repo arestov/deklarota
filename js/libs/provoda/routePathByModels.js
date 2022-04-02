c7623a321c7e948fda80e6ed51f6263770ea2a66
Gleb Arestov <gr1b0k@gmail.com>
2022-04-02 17:03:53 +0300

    refactor: remove dead code in routePathByModels

--- js/libs/provoda/routePathByModels.js
+++ js/libs/provoda/routePathByModels.js
@@ -1,5 +1,4 @@
 
-import allStates from './dcl/routes/allStates'
 import getModernPage from './dcl/routes/getByName'
 import createModern, { getRouteConstr, selectModern } from './dcl/routes/createModern'
 import { toBasicTemplate } from './routes/parse'
@@ -104,15 +103,6 @@ const routePathByModels = function routePathByModels(start_md, pth_string, need_
   return result
 }
 
-function slash(str) {
-  return str.split('/')
-}
-
-function selectRouteItem(self, sp_name) {
-  if (self.subPager) {
-    throw new Error('`subPager` is legacy. (there is no proper way to get `constr`, only `instance`). so get rid of `subPager`')
-  }
-}
 
 function findModern(self, sp_name, options) {
   if (self.__routes_matchers_defs == null) {
@@ -134,87 +124,10 @@ function findModern(self, sp_name, options) {
 }
 
 function getterSPI() {
-
-  const prepare = function(self, item, sp_name, slashed, extra_states) {
-    const Constr = self._all_chi[item.key]
-    /*
-
-    берем данные из родителя
-    накладываем стандартные данные
-    накладываем данные из урла
-    */
-
-    const byType = item.byType
-
-    const normal_part = byType ? slashed.slice(1) : slashed
-    const by_colon = normal_part[0].split(':').map(decodeURIComponent)
-    const by_comma = normal_part[0].split(',').map(decodeURIComponent)
-    const by_slash = normal_part.map(decodeURIComponent)
-
-    const states = {
-      url_part: '/' + sp_name
-    }
-
-    const hbu_data = {
-      simple_name: sp_name,
-      decoded_name: decodeURIComponent(sp_name),
-      name_spaced: by_colon[1],
-      by_comma: by_comma,
-      by_colon: by_colon,
-      by_slash: by_slash,
-    }
-
-    const hbu_declr = item.getHead
-    const morph_helpers = self.app.morph_helpers
-
-    const head_by_urlname = hbu_declr && hbu_declr(hbu_data, null, morph_helpers)
-
-    if (!Constr.prototype.handling_v2_init) {
-      throw new Error('handling_v2_init = false')
-    }
-
-    return self.initSi(Constr, {
-      by: 'routePathByModels',
-      init_version: 2,
-      attrs: allStates(states, extra_states),
-      head: head_by_urlname,
-      url_params: hbu_data,
-    })
-  }
-
   return function getSPI(self, sp_name, options, extra_states) {
-    const autocreate = !options || options.autocreate !== false
-
     const modern = findModern(self, sp_name, options)
     if (modern) {
       return modern
-    }
-
-    const item = selectRouteItem(self, sp_name)
-    if (item != null) {
-
-      const getKey = item.getKey
-      const key = getKey ? getKey(decodeURIComponent(sp_name), sp_name) : sp_name
-
-      if (self.sub_pages && self.sub_pages[key]) {
-        if (self.sub_pages[key] === MARKED_REMOVED) {
-          return null
-        }
-        return self.sub_pages[key]
-      }
-
-      if (!autocreate) {
-        return null
-      }
-
-      const instance = item && prepare(self, item, sp_name, slash(sp_name), extra_states)
-      if (instance) {
-        watchModelDie(self, instance)
-        watchSubPageKey(self, instance, key)
-        self.sub_pages[key] = instance
-
-        return instance
-      }
     }
   }
 }
@@ -224,11 +137,6 @@ function getterSPIConstr() {
     const modern = selectModern(self, sp_name)
     if (modern) {
       return modern.Constr
-    }
-
-    const item = selectRouteItem(self, sp_name)
-    if (item) {
-      return self._all_chi[item.key]
     }
 
   }
@@ -256,23 +164,6 @@ function watchModelDie(self, instance) {
 
     sub_pages = null
   })
-}
-
-function watchSubPageKey(self, instance, key) {
-  if (!instance.hasComplexStateFn('url_part')) {
-    return
-  }
-
-  let cur_key = key
-
-  self.lwch(instance, 'url_part', function(value) {
-    if (self.sub_pages[cur_key] === instance) {
-      self.sub_pages[cur_key] = null
-    }
-    self.sub_pages[value] = instance
-    cur_key = value
-  })
-
 }
 
 routePathByModels.getSPI = getSPI
