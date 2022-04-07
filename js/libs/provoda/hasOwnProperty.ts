7d19e9cdaadd15b28954eecc3eea681cac8bb8f0
Gleb Arestov <gr1b0k@gmail.com>
2022-04-07 16:13:39 +0300

    chore: add more validations for ts

--- js/libs/provoda/hasOwnProperty.ts
+++ js/libs/provoda/hasOwnProperty.ts
@@ -1,3 +1,3 @@
-export function hasOwnProperty(obj: object, prop: string) {
+export function hasOwnProperty(obj: object, prop: string): boolean {
   return Object.prototype.hasOwnProperty.call(obj, prop)
 }
