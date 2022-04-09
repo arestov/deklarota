c77f23b6c44d32e223ced49db60b20d39708f6e3
Gleb Arestov <gr1b0k@gmail.com>
2022-04-09 13:01:10 +0300

    refactor: convert MentionChain to class

--- js/libs/provoda/Model/mentions/MentionChain.js
+++ js/libs/provoda/Model/mentions/MentionChain.js
@@ -1,5 +1,14 @@
 import target_types from './target_types'
 
+class MentionChainLink {
+  constructor(chain, num, rel) {
+    this.chain = chain
+    this.num = num
+    this.rel = rel
+    Object.freeze(this)
+  }
+}
+
 function addrToLinks(rel_path, chain) {
   const list = []
 
@@ -13,37 +22,32 @@ function addrToLinks(rel_path, chain) {
   return list
 }
 
-function MentionChainLink(chain, num, rel) {
-  this.chain = chain
-  this.num = num
-  this.rel = rel
-  Object.freeze(this)
-}
+class MentionChain {
+  constructor(target_type, rel_path, target_matcher, addr, target_name, handler_payload) {
+    this.target_matcher = target_matcher
+    this.target_type = target_type
+    this.addr = addr
+    this.list = addrToLinks(rel_path, this)
+    switch (target_type) {
+      case target_types.TARGET_TYPE_ATTR:
+      case target_types.TARGET_TYPE_GLUE_REL: {
+        if (!target_name) {
+          throw new Error('target_name should be provided')
+        }
+        break
+      }
+      case target_types.TARGET_TYPE_HEAVY_REQUESTER: {
+        if (!handler_payload) {
+          throw new Error('handler_payload should be provided')
+        }
+      }
+      default:
+    }
 
-function MentionChain(target_type, rel_path, target_matcher, addr, target_name, handler_payload) {
-  this.target_matcher = target_matcher
-  this.target_type = target_type
-  this.addr = addr
-  this.list = addrToLinks(rel_path, this)
-  switch (target_type) {
-    case target_types.TARGET_TYPE_ATTR:
-    case target_types.TARGET_TYPE_GLUE_REL: {
-      if (!target_name) {
-        throw new Error('target_name should be provided')
-      }
-      break
-    }
-    case target_types.TARGET_TYPE_HEAVY_REQUESTER: {
-      if (!handler_payload) {
-        throw new Error('handler_payload should be provided')
-      }
-    }
-    default:
+    this.target_name = target_name || ''
+    this.handler_payload = handler_payload || null
+    Object.freeze(this)
   }
-
-  this.target_name = target_name || ''
-  this.handler_payload = handler_payload || null
-  Object.freeze(this)
 }
 
 
