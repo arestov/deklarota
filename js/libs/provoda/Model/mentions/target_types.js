376fc37e5f75a80ec87d89a50e558dd8551d4968
Gleb Arestov <gr1b0k@gmail.com>
2022-04-10 09:10:47 +0300

    refactor: use constant values for target_types

--- js/libs/provoda/Model/mentions/target_types.js
+++ js/libs/provoda/Model/mentions/target_types.js
@@ -1,10 +1,9 @@
 
-let type_counter = 0
-const TARGET_TYPE_ATTR = type_counter++
-const TARGET_TYPE_GLUE_REL = type_counter++
-const TARGET_TYPE_HEAVY_REQUESTER = type_counter++
-const TARGET_TYPE_UNIQ_REL_BY_ATTR = type_counter++
-const TARGET_TYPE_ROUTE_MATCHING = type_counter++
+const TARGET_TYPE_ATTR = 0
+const TARGET_TYPE_GLUE_REL = 1
+const TARGET_TYPE_HEAVY_REQUESTER = 2
+const TARGET_TYPE_UNIQ_REL_BY_ATTR = 3
+const TARGET_TYPE_ROUTE_MATCHING = 4
 
 export default {
   TARGET_TYPE_ATTR,
