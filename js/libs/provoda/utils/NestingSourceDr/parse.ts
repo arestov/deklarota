de740a293e00b5b2fa7809745f86691c5bb82f5a
Gleb Arestov <gr1b0k@gmail.com>
2022-04-10 17:39:26 +0300

    refactor: convert NestingSourceDr\parse to ts

---
+++ js/libs/provoda/utils/NestingSourceDr/parse.ts
@@ -0,0 +1,21 @@
+
+import memorize from '../../../spv/memorize'
+import splitByDot from '../../../spv/splitByDot'
+
+class NestingSourceDr {
+  start_point: string | false
+  selector: string[]
+  constructor(string: string) {
+    const parts = string.split('>')
+    this.start_point = parts.length > 1 && (parts[0] || false)
+    const last_part = parts[parts.length - 1]
+    if (last_part == null) {
+      throw new Error('provide last part of path')
+    }
+    this.selector = splitByDot(last_part)
+  }
+}
+
+export default memorize(function parseNSD(string) {
+  return new NestingSourceDr(string)
+})
