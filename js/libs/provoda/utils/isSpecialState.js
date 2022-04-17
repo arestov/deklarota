5e3a5fdf7bf74e4d861a02ae5c210b1ff0c03f06
Gleb Arestov <gr1b0k@gmail.com>
2022-04-17 15:39:27 +0300

    refactor: use switch in isSpecialState

--- js/libs/provoda/utils/isSpecialState.js
+++ js/libs/provoda/utils/isSpecialState.js
@@ -1,13 +1,20 @@
 
 import spv from '../../spv'
 
-const spec_chars = { '^': true, '@': true, '#': true, '<': true }
 const isSpecialState = spv.memorize(function(state_name) {
   const char = state_name.charAt(0)
-  if (char === '&') {
-    throw new Error('require marks should not be passed here. cut it earlier')
+
+  switch (char) {
+    case '&':
+      throw new Error('require marks should not be passed here. cut it earlier')
+    case '^':
+    case '@':
+    case '#':
+    case '<':
+      return true
   }
-  return spec_chars[state_name.charAt(0)]
+
+  return false
 })
 
 export default isSpecialState
