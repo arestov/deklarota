7d2ea79c80497ed51d7712851cdec711bcaef744
Gleb Arestov <gr1b0k@gmail.com>
2022-04-23 17:55:04 +0300

    refactor: state_req/rebuild

--- js/libs/provoda/dcl/effects/legacy/state_req/rebuild.js
+++ js/libs/provoda/dcl/effects/legacy/state_req/rebuild.js
@@ -17,30 +17,33 @@ const doIndex = function(list, value) {
 
 
 export default function buildStateReqs(self, list) {
-  self._states_reqs_index = {}
   self._states_reqs_list = list
-  const states_index = {}
 
+  const _states_reqs_index = {}
+  const uniq_states = new Set()
   for (let i = 0; i < list.length; i++) {
     const states_list = list[i].states_list
     for (let jj = 0; jj < states_list.length; jj++) {
-      states_index[states_list[jj]] = true
+      uniq_states.add(states_list[jj])
     }
   }
-  for (const state_name in states_index) {
-    self._states_reqs_index[state_name] = doIndex(list, state_name)
+  for (const state_name of uniq_states) {
+    _states_reqs_index[state_name] = doIndex(list, state_name)
   }
+  self._states_reqs_index = _states_reqs_index
 
-  self.netsources_of_states = {
+  const netsources_of_states = {
     api_names: [],
     api_names_converted: false,
     sources_names: []
   }
 
   for (let i = 0; i < list.length; i++) {
-    changeSources(self.netsources_of_states, list[i].send_declr)
+    changeSources(netsources_of_states, list[i].send_declr)
 
     // copy dependencies to comp, so runtime will subscribe to nonlocal changes
     // (todo: subscribe to nonlocal deps without mutating comp)
   }
+
+  self.netsources_of_states = netsources_of_states
 }
