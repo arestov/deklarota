8b2026c70b50f4e327ae5f8a8e48b4c82feb5899
Gleb Arestov <gr1b0k@gmail.com>
2022-04-23 16:43:36 +0300

    refactor: make fn for netsources_of_nestings

--- js/libs/provoda/dcl/effects/legacy/nest_req/rebuild.js
+++ js/libs/provoda/dcl/effects/legacy/nest_req/rebuild.js
@@ -1,21 +1,27 @@
 
 import changeSources from '../utils/changeSources'
 
-export default function buildNestReqs(self, by_name) {
-  self._nest_reqs = by_name
-
-  self.netsources_of_nestings = {
+const getSource = (by_name) => {
+  const netsources_of_nestings = {
     api_names: [],
     api_names_converted: false,
     sources_names: []
   }
 
-  for (const nest_name in self._nest_reqs) {
-    const cur_nest = self._nest_reqs[nest_name]
-    changeSources(self.netsources_of_nestings, cur_nest.send_declr)
+  for (const nest_name in by_name) {
+    const cur_nest = by_name[nest_name]
+    changeSources(netsources_of_nestings, cur_nest.send_declr)
 
     if (!cur_nest.state_dep) {
       continue
     }
   }
+
+  return netsources_of_nestings
+}
+
+export default function buildNestReqs(self, by_name) {
+  self._nest_reqs = by_name
+
+  self.netsources_of_nestings = getSource(by_name)
 }
