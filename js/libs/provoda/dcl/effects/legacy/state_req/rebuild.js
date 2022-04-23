5fca313f8a958c74e3371d14d2eb9b2df3b392b2
Gleb Arestov <gr1b0k@gmail.com>
2022-04-23 18:08:17 +0300

    refactor: use schema for state_req/rebuild

--- js/libs/provoda/dcl/effects/legacy/state_req/rebuild.js
+++ js/libs/provoda/dcl/effects/legacy/state_req/rebuild.js
@@ -1,5 +1,6 @@
 
 
+import { cacheFields } from '../../../cachedField'
 import changeSources from '../utils/changeSources'
 
 const doIndex = function(list, value) {
@@ -15,35 +16,52 @@ const doIndex = function(list, value) {
   return result
 }
 
+const _states_reqs_index = [
+  ['_states_reqs_list'],
+  (list) => {
+    const result = {}
+    const uniq_states = new Set()
+    for (let i = 0; i < list.length; i++) {
+      const states_list = list[i].states_list
+      for (let jj = 0; jj < states_list.length; jj++) {
+        uniq_states.add(states_list[jj])
+      }
+    }
+    for (const state_name of uniq_states) {
+      result[state_name] = doIndex(list, state_name)
+    }
+    return result
+  }
+]
+
+const netsources_of_states = [
+  ['_states_reqs_list'],
+  (list) => {
+    const result = {
+      api_names: [],
+      api_names_converted: false,
+      sources_names: []
+    }
+
+    for (let i = 0; i < list.length; i++) {
+      changeSources(result, list[i].send_declr)
+
+      // copy dependencies to comp, so runtime will subscribe to nonlocal changes
+      // (todo: subscribe to nonlocal deps without mutating comp)
+    }
+
+    return result
+  }
+]
+
+const schema = {
+  _states_reqs_index,
+  netsources_of_states,
+}
+
 
 export default function buildStateReqs(self, list) {
   self._states_reqs_list = list
 
-  const _states_reqs_index = {}
-  const uniq_states = new Set()
-  for (let i = 0; i < list.length; i++) {
-    const states_list = list[i].states_list
-    for (let jj = 0; jj < states_list.length; jj++) {
-      uniq_states.add(states_list[jj])
-    }
-  }
-  for (const state_name of uniq_states) {
-    _states_reqs_index[state_name] = doIndex(list, state_name)
-  }
-  self._states_reqs_index = _states_reqs_index
-
-  const netsources_of_states = {
-    api_names: [],
-    api_names_converted: false,
-    sources_names: []
-  }
-
-  for (let i = 0; i < list.length; i++) {
-    changeSources(netsources_of_states, list[i].send_declr)
-
-    // copy dependencies to comp, so runtime will subscribe to nonlocal changes
-    // (todo: subscribe to nonlocal deps without mutating comp)
-  }
-
-  self.netsources_of_states = netsources_of_states
+  cacheFields(schema, self)
 }
