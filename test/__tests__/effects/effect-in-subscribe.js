2eb1c34beb0c01def222a0669992e19ec11f6546
Gleb Arestov <gr1b0k@gmail.com>
2022-04-25 15:28:37 +0300

    test: effects.in.subscribe

---
+++ test/__tests__/effects/effect-in-subscribe.js
@@ -0,0 +1,97 @@
+/* eslint-disable fp/no-let */
+/* eslint-disable fp/no-mutation */
+/* eslint no-lone-blocks: 0 */
+import { expect, test } from '@jest/globals'
+
+import model from 'pv/model'
+
+import modernRoot from '../../modernRoot'
+import testingInit from '../../testingInit'
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+test('should use api from root & parent', async () => {
+  const User = model({
+    model_name: 'User',
+    attrs: {
+      attr1: ['input'],
+    },
+    actions: {
+      action1: {
+        to: ['attr1'],
+      },
+      action2: {
+        to: ['prop2'],
+      },
+    },
+    effects: {
+      api: {
+        localApiFromRoot: [
+          ['_provoda_id'],
+          ['#someApi'],
+          api => api,
+        ],
+        // localApiFromParent: [
+        //   ['_provoda_id'],
+        //   ['#someApi'],
+        //   api => api,
+        // ],
+      },
+      in: {
+        action1: {
+          type: 'subscribe',
+          api: ['localApiFromRoot'],
+          fn: (dispatch, api) => {
+            api.onChange1(dispatch)
+
+            return () => {}
+          },
+        },
+
+        // action2: {
+        //   type: 'subscribe',
+        //   api: ['localApiFromParent'],
+        //   fn: (dispatch, api) => {
+        //     api.onChange1(dispatch)
+
+        //     return () => {}
+        //   },
+        // },
+      },
+    },
+
+
+  })
+
+  const AppRoot = modernRoot({
+    attrs: {},
+    rels: {
+      user: ['nest', [User]],
+    },
+    checkActingRequestsPriority: () => {},
+  })
+
+  const inited = await testingInit(AppRoot)
+  const { computed } = inited
+
+  const value1 = 'value1-ljfalsjdf88'
+
+  const api = {
+    onChange1: fn => {
+      setTimeout(() => {
+        fn(value1)
+      }, 0)
+    },
+  }
+  inited.app_model.useInterface('someApi', api)
+
+  await computed()
+  expect(inited.app_model.readAddr('< @one:attr1 < user')).toBe(undefined)
+
+  {
+    await wait(500)
+    await computed()
+
+    expect(inited.app_model.readAddr('< @one:attr1 < user')).toBe(value1)
+  }
+})
