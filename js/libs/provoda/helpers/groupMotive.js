911604e86ec6f256105f35269fb19e7de6b53e79
Gleb Arestov <gr1b0k@gmail.com>
2022-05-05 19:30:57 +0300

    refactor: assert current motivator

--- js/libs/provoda/helpers/groupMotive.js
+++ js/libs/provoda/helpers/groupMotive.js
@@ -3,18 +3,9 @@
 export default function(fn) {
   return function() {
     const self = this
-    const need = !this._currentMotivator()
-    if (!need) {
-      return fn.apply(self, arguments)
+    if (!this._currentMotivator()) {
+      throw new Error('should have motivator')
     }
-
-    const flow = self._getCallsFlow()
-    const motivator = flow.startGroup()
-    self.current_motivator = motivator
-    const result = fn.apply(self, arguments)
-    flow.completeGroup(motivator)
-    self.current_motivator = null
-    flow.checkCallbacksFlow()
-    return result
+    return fn.apply(self, arguments)
   }
 };
