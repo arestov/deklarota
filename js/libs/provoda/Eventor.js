efe67f71c3a80021c3833cb3b4d538f1be58e762
Gleb Arestov <gr1b0k@gmail.com>
2022-05-06 12:09:16 +0300

    refactor: move code

--- js/libs/provoda/Eventor.js
+++ js/libs/provoda/Eventor.js
@@ -13,6 +13,53 @@ const assertCurm = (self) => {
   }
 }
 
+const flows = {
+  onDie: function(cb) {
+    subscribeToDie(this, cb)
+  },
+  _getCallsFlow: function() {
+    // disable this._local_calls_flow || for some time!
+    return this._calls_flow
+  },
+  _currentMotivator: function() {
+    return this._getCallsFlow().current_step
+  },
+  input: function(fn, args) {
+    this._getCallsFlow().input(fn, args)
+  },
+  inputFn: function(fn) {
+    const self = this
+    return function() {
+      const args = Array.prototype.slice.call(arguments)
+      self._calls_flow.pushToFlow(fn, self, args)
+    }
+  },
+  nextLocalTick: function(fn, args, use_current_motivator, finup) {
+    if (!use_current_motivator) {
+      throw new Error('consider to use .input() or .inputWithContext()')
+    }
+
+    assertCurm(this)
+
+    return this._getCallsFlow().pushToFlow(fn, this, args, false, WFlowStepWrapper, this, use_current_motivator && this._currentMotivator(), finup)
+  },
+  inputWithContext: function(fn, args) {
+    this._getCallsFlow().input(fn, args, this)
+  },
+  nextTick: function(fn, args, use_current_motivator, initiator) {
+    if (!use_current_motivator) {
+      throw new Error('consider to use .input() or .inputWithContext()')
+    }
+
+    assertCurm(this)
+
+    return this._calls_flow.pushToFlow(
+      fn, this, args, !args && this, WFlowStepWrapper, this, use_current_motivator && this._currentMotivator(), false,
+      initiator, fn.init_end
+    )
+  },
+}
+
 const Eventor = spv.inh(function() {}, {
   naming: function(construct) {
     return function Eventor() {
@@ -24,54 +71,12 @@ const Eventor = spv.inh(function() {}, {
     self.evcompanion = new FastEventor(self)
   },
   props: {
-    onDie: function(cb) {
-      subscribeToDie(this, cb)
-    },
     // init: function(){
     // 	this.evcompanion = new FastEventor(this);
     // 	return this;
     // },
-    _getCallsFlow: function() {
-      // disable this._local_calls_flow || for some time!
-      return this._calls_flow
-    },
-    _currentMotivator: function() {
-      return this._getCallsFlow().current_step
-    },
-    input: function(fn, args) {
-      this._getCallsFlow().input(fn, args)
-    },
-    inputFn: function(fn) {
-      const self = this
-      return function() {
-        const args = Array.prototype.slice.call(arguments)
-        self._calls_flow.pushToFlow(fn, self, args)
-      }
-    },
-    nextLocalTick: function(fn, args, use_current_motivator, finup) {
-      if (!use_current_motivator) {
-        throw new Error('consider to use .input() or .inputWithContext()')
-      }
 
-      assertCurm(this)
-
-      return this._getCallsFlow().pushToFlow(fn, this, args, false, WFlowStepWrapper, this, use_current_motivator && this._currentMotivator(), finup)
-    },
-    inputWithContext: function(fn, args) {
-      this._getCallsFlow().input(fn, args, this)
-    },
-    nextTick: function(fn, args, use_current_motivator, initiator) {
-      if (!use_current_motivator) {
-        throw new Error('consider to use .input() or .inputWithContext()')
-      }
-
-      assertCurm(this)
-
-      return this._calls_flow.pushToFlow(
-        fn, this, args, !args && this, WFlowStepWrapper, this, use_current_motivator && this._currentMotivator(), false,
-        initiator, fn.init_end
-      )
-    },
+    ...flows,
     off: function(namespace, cb, obj, context) {
       return this.evcompanion.off(namespace, cb, obj, context)
     },
