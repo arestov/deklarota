d33834fc79f35d90faf98b7346e33ec81e4a9f8d
Gleb Arestov <gr1b0k@gmail.com>
2023-01-30 09:44:30 +0400

    feat: allow to use $root in expectRelBeRevealedByRelPath

--- js/models/handleExpectedRelChange.js
+++ js/models/handleExpectedRelChange.js
@@ -15,7 +15,14 @@ const handleExpectedRelChange = (chain, current_md) => {
     const requesting_action = `requireRel:${rel_name}`
     const action_dcl = getAction(link_step_value, requesting_action)
 
-    if (!action_dcl) {
+    if (!action_dcl && rel_name !== '$root') {
+      /*
+        it's ok to ask `$root`
+        it's not ok to ask `$parent` (it's kinda legacy/unstable, better to avoid)
+        it's not ok to ask something we can't request
+
+        todo: it could be ok to ask rel-nest (garanteed to present rels)
+      */
       const err = new Error('impossible to request')
       console.error({rel_name}, err)
       throw err
@@ -26,6 +33,13 @@ const handleExpectedRelChange = (chain, current_md) => {
       link_step_value = value
       complete = i == chain.list.length - 1
       continue
+    }
+
+    if (!action_dcl) {
+      /* it's impossible that we don't have $root. but anyway let's check it */
+      const err = new Error('missing rel that should be preset')
+      console.error({rel_name}, err)
+      throw err
     }
 
     const self = link_step_value
