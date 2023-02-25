f07c0f7fdd4be82175b4f4f6f9b60d8286f707bb
Gleb Arestov <gr1b0k@gmail.com>
2023-02-25 13:23:20 +0400

    refactor: convert CallbacksFlow typescript

--- js/libs/provoda/CallbacksFlow.ts
+++ js/libs/provoda/CallbacksFlow.ts
@@ -4,27 +4,43 @@ import FlowStep from './FlowStep'
 import orderFlow from './CallbacksFlow/order'
 import { getBoxedSetImmFunc } from './CallbacksFlow/getBoxedSetImmFunc'
 import { getBoxedRAFFunc } from './CallbacksFlow/getBoxedRAFFunc'
+import type { TickFn } from './CallbacksFlow/tick.types'
 
 const MIN = 60 * 1000
 
 class CallbacksFlow {
-  constructor(options) {
+  // private flow: FlowStep[] = [];
+  public flow_start: FlowStep | null = null;
+  public flow_end: FlowStep | null = null;
+  private callbacks_busy = false;
+  private iteration_time: number;
+  private iteration_delayed_check_time = 0;
+  private flow_steps_counter = 1;
+  private bad_stops_strike_counter = 0;
+  private current_step: FlowStep | null = null;
+  hndIterateCallbacksFlow: () => void
+  callFlowStep: (flowStep: FlowStep, aborted?: boolean) => void
+  onFinalTransactionStep: ((flowStep: FlowStep) => void)
+  pushIteration: TickFn
+  reportLongTask: ((step: FlowStep, duration: number) => void) | null
+  reportHugeQueue: ((step: FlowStep, duration: number) => void) | null
+  constructor(options: {
+    callFlowStep: (flowStep: FlowStep, aborted?: boolean) => void;
+    glo: Window | typeof globalThis;
+    rendering_flow?: boolean;
+    iteration_time?: number;
+    onError: (error: Error) => void;
+    reportLongTask?: (step: FlowStep, duration: number) => void;
+    reportHugeQueue?: (step: FlowStep, duration: number) => void;
+    onFinalTransactionStep: (flowStep: FlowStep) => void;
+  }) {
     this.callFlowStep = options.callFlowStep
-  // glo is global/window
     const glo = options.glo
     const rendering_flow = options.rendering_flow
     const iteration_time = options.iteration_time
 
-    this.flow = []
-    this.flow_start = null
-    this.flow_end = null
-    this.busy = false
     this.iteration_time = iteration_time || 250
-    this.iteration_delayed_check_time = 0
-    this.flow_steps_counter = 1
-    this.bad_stops_strike_counter = 0
 
-  // this.flow_steps_collating_invalidated = null;
     this.hndIterateCallbacksFlow = this.iterateCallbacksFlow.bind(this)
 
     const raf = rendering_flow && getBoxedRAFFunc(glo)
@@ -37,13 +53,17 @@ class CallbacksFlow {
 
     this.reportLongTask = options.reportLongTask || null
     this.reportHugeQueue = options.reportHugeQueue || null
-
     this.onFinalTransactionStep = options.onFinalTransactionStep
   }
-  input(fn, args, context) {
+
+  input(
+    fn: Function,
+    args: unknown[] | undefined,
+    context: unknown | null,
+  ): void {
     this.pushToFlow(fn, context, args)
   }
-  iterateCallbacksFlow() {
+  iterateCallbacksFlow(): void {
     const started_at = Date.now()
     const start = started_at + this.iteration_time
     let last_call_at = started_at
@@ -98,7 +118,7 @@ class CallbacksFlow {
       // clean
       toClean.next = null
     }
-    this.flow_start = stopped
+    this.flow_start = stopped || null
     if (!stopped) {
       this.bad_stops_strike_counter = 0
       this.flow_end = null
@@ -116,7 +136,7 @@ class CallbacksFlow {
     }
 
   }
-  checkCallbacksFlow() {
+  checkCallbacksFlow(): void {
     if (this.callbacks_busy) {
       return
     }
@@ -137,14 +157,30 @@ class CallbacksFlow {
     this.pushIteration(this.hndIterateCallbacksFlow)
     this.iteration_delayed_check_time = now + MIN
   }
-  pushToFlowWithMotivator(fn, context, args, force) {
+  pushToFlowWithMotivator(
+    fn: Function,
+    context: unknown | null,
+    args: unknown[] | undefined,
+    force?: boolean
+  ): void {
     const motivator = this.current_step
     if (!motivator && force) {
       throw new Error('missing motivator')
     }
     this.pushToFlow(fn, context, args, null, null, null, motivator)
   }
-  pushToFlow(fn, context, args, cbf_arg, cb_wrapper, real_context, motivator, finup, initiator, init_end) {
+  pushToFlow(
+    fn: Function,
+    context: unknown,
+    args: unknown[] | undefined,
+    cbf_arg?: null,
+    cb_wrapper?: null | Function,
+    real_context?: unknown,
+    motivator?: FlowStep | null,
+    finup?: boolean,
+    initiator?: unknown, // remove initiator argument as it's not used and throw an error for it
+    init_end?: undefined // remove init_end argument as it's not used and throw an error for it
+  ): FlowStep {
     const flow_step_num = ++this.flow_steps_counter
 
     const complex_order = complexOrder(motivator?.complex_order, flow_step_num)
@@ -155,36 +191,64 @@ class CallbacksFlow {
 
 
     const is_transaction_end = motivator ? motivator.is_transaction_end : false
-    const flow_step = new FlowStep(this.callFlowStep, is_transaction_end, flow_step_num, complex_order, fn, context, args, cbf_arg, cb_wrapper, real_context, finup, init_end)
+    const flow_step = new FlowStep(
+      this.callFlowStep,
+      is_transaction_end,
+      flow_step_num,
+      complex_order,
+      fn,
+      context,
+      args,
+      cbf_arg || null,
+      cb_wrapper || null,
+      real_context || null,
+      finup,
+      init_end
+    )
     orderFlow(this, flow_step)
     this.checkCallbacksFlow()
     return flow_step
 
   }
-  scheduleTransactionEnd(starter_id, context, args, fn, cb_wrapper) {
+  scheduleTransactionEnd(
+    starter_id: number,
+    context: unknown,
+    args: unknown[] | undefined,
+    fn: Function,
+    cb_wrapper: Function | undefined
+  ): void {
     const flow_step_num = ++this.flow_steps_counter
     const complex_order = [starter_id, Infinity, flow_step_num]
 
     const is_transaction_end = true
-    const flow_step = new FlowStep(this.callFlowStep, is_transaction_end, flow_step_num, complex_order, fn, context, args, null, cb_wrapper)
+    const flow_step = new FlowStep(
+      this.callFlowStep,
+      is_transaction_end,
+      flow_step_num,
+      complex_order,
+      fn,
+      context,
+      args,
+      null,
+      cb_wrapper || null
+    )
     orderFlow(this, flow_step)
     this.checkCallbacksFlow()
   }
-  handleFinalTrasactionEnd(step) {
+  handleFinalTrasactionEnd(step: FlowStep): void {
     this.onFinalTransactionStep(step)
   }
-  reportLongTaskRaw(taskTime, task) {
+  reportLongTaskRaw(taskTime: number, task: FlowStep): void {
     if (taskTime < 500) {
       return
     }
     const reportLongTask = this.reportLongTask
-    /* for typescript */
     if (reportLongTask == null) {
       return
     }
     reportLongTask(task, taskTime)
   }
-  reportHugeQueueRaw(duration, task) {
+  reportHugeQueueRaw(duration: number, task: FlowStep): void {
     if (this.reportHugeQueue == null) {
       return
     }
@@ -194,7 +258,7 @@ class CallbacksFlow {
   }
 }
 
-function complexOrder(order_list, new_step_num) {
+function complexOrder(order_list: readonly number[] | undefined, new_step_num: number): readonly number[] {
   if (!order_list) {
     return Object.freeze([new_step_num])
 
